37efdaf2a559d4e08d52f9d99bb18eb916f3da60
Zaccharie Attias <zaccharieattias@gmail.com>
2023-09-29 11:53:27 +0200

    Initial commit

---
+++ Assets/Scripts/Move_Player.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UIElements;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private bool isMoving;
+
+    private Vector2 input;
+
+    private Animator animator;
+
+    public LayerMask solidObjectsLayer;
+
+    private void Awake(){
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            Debug.Log("This is input.x"+input.x);
+            Debug.Log("This is input.y"+input.y);
+
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if(IsWalkable(targetPos))
+                    StartCoroutine(Move(targetPos));
+            }
+
+        }
+
+        animator.SetBool("isMoving",isMoving);
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPos){
+        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer)!= null){
+            return false;}
+        return true;
+    }
+
+}
