cc32ba61dab184c0f38ff3a97210e0a75eebac25
Zaccharie Attias <zaccharieattias@gmail.com>
2023-09-29 12:36:11 +0200

    Collision fix

--- Assets/Scripts/Move_Player.cs
+++ Assets/Scripts/Move_Player.cs
@@ -9,11 +9,17 @@ public class PlayerController : MonoBehaviour
 
     private bool isMoving;
 
-    private Vector2 input;
+    private Vector2 input; //direction from the input
 
     private Animator animator;
 
     public LayerMask solidObjectsLayer;
+
+    private Rigidbody2D rb;
+
+    private void Start(){
+        rb = GetComponent<Rigidbody2D>();
+     }
 
     private void Awake(){
         animator = GetComponent<Animator>();
@@ -26,22 +32,17 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
-            Debug.Log("This is input.x"+input.x);
-            Debug.Log("This is input.y"+input.y);
-
             if (input.x != 0) input.y = 0;
 
             if (input != Vector2.zero)
             {
+                isMoving = true;
+
                 animator.SetFloat("moveX", input.x);
                 animator.SetFloat("moveY", input.y);
 
-                var targetPos = transform.position;
-                targetPos.x += input.x;
-                targetPos.y += input.y;
-
-                if(IsWalkable(targetPos))
-                    StartCoroutine(Move(targetPos));
+                rb.MovePosition(rb.position + input * moveSpeed * Time.fixedDeltaTime);
+                isMoving = false;
             }
 
         }
@@ -49,24 +50,5 @@ public class PlayerController : MonoBehaviour
         animator.SetBool("isMoving",isMoving);
     }
 
-    IEnumerator Move(Vector3 targetPos)
-    {
-        isMoving = true;
-
-        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
-            yield return null;
-        }
-        transform.position = targetPos;
-
-        isMoving = false;
-    }
-
-    private bool IsWalkable(Vector3 targetPos){
-        if(Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectsLayer)!= null){
-            return false;}
-        return true;
-    }
 
 }
