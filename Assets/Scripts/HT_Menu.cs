ae864dcbdfbf2b7670eca69ca5cc638b9b42cde5
Zaccharie Attias <zaccharieattias@gmail.com>
2023-11-16 21:43:43 +0100

    Fix script definition

--- Assets/Scripts/HT_Menu.cs
+++ Assets/Scripts/HT_Menu.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GP_screen : MonoBehaviour
+public class HT_Menu : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
