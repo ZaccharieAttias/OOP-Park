1c84c3f4350a12fd1f915dc8957fa230f2cac609
Zaccharie Attias <zaccharieattias@gmail.com>
2023-11-26 20:30:11 +0100

    Respawn

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    Vector2 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Respawn();
+    }
+
+    void Respawn()
+    {
+        transform.position = startPos;
+    }
+}
