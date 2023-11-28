701685fcaf85010a2427d8f47e5d108cdfec5bef
Zaccharie Attias <zaccharieattias@gmail.com>
2023-11-28 21:46:50 +0100

    Sticky moving platform

---
+++ Assets/Scripts/StickyPlatform.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StickyPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.parent = null;
+        }
+    }
+
+}
