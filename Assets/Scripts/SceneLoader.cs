953fa234b081caaef0c31611a2be4a6cee5cb700
Nitzan <nitzantomer1998@gmail.com>
2023-12-03 14:21:30 +0200

    Adding Scene Loader Script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneLoader : MonoBehaviour
+{
+    public void Presentation()
+    {
+        SceneManager.LoadScene("Presentation");
+    }
+
+    public void Playground()
+    {
+        SceneManager.LoadScene("Playground");
+    }
+
+    public void LocalPark()
+    {
+        SceneManager.LoadScene("LocalPark");
+    }
+
+    public void OnlinePark()
+    {
+        SceneManager.LoadScene("OnlinePark");
+    }
+
+    public void Progression()
+    {
+        SceneManager.LoadScene("Progression");
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void Hierarchy()
+    {
+        SceneManager.LoadScene("Hierarchy");
+    }
+
+    public void Polymorphism()
+    {
+        SceneManager.LoadScene("Polymorphism");
+    }
+}
