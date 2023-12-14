d47039230e3d01851e8e679323418df70f2f7c2f
Zaccharie Attias <zaccharieattias@gmail.com>
2023-12-14 20:01:32 +0100

    add pink script into the script changing skin

--- Assets/Scripts/Character/ChangingSkin.cs
+++ Assets/Scripts/Character/ChangingSkin.cs
@@ -6,13 +6,19 @@ public class ChangingSkin : MonoBehaviour
 {
     public AnimatorOverrideController bandw;
     public AnimatorOverrideController normal;
+    public AnimatorOverrideController pink;
 
     public void BlackAndWhiteSkin()
     {
         GetComponent<Animator>().runtimeAnimatorController = bandw as RuntimeAnimatorController;
     }
 
-        public void NormalSkin()
+    public void NormalSkin()
+    {
+        GetComponent<Animator>().runtimeAnimatorController = normal as RuntimeAnimatorController;
+    }
+
+    public void PinkSkin()
     {
         GetComponent<Animator>().runtimeAnimatorController = normal as RuntimeAnimatorController;
     }
