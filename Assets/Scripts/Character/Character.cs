09979df324401cf4f05365e880e6c1464d2aaecb
Nitzan <nitzantomer1998@gmail.com>
2023-12-27 23:35:45 +0200

    Updating Project

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,47 +1,39 @@
-using UnityEngine;
 using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.IO;
-using System.Linq;
-using System;
+using UnityEngine.EventSystems;
+using System.Collections;
 
 
 [System.Serializable]
-public class Character : MonoBehaviour
+public class Character
 {
     public string name;
-    public List<Character> ancestors;
+    public List<Character> parents;
     public List<Character> childrens;
     public List<CharacterAttribute> attributes;
     public List<CharacterMethod> methods;
     public string description;
-    public int depth = 0;
+    public int depth;
 
 
-    public Character()
-    {
-        ancestors = new List<Character>();
-        childrens = new List<Character>();
-        attributes = new List<CharacterAttribute>();
-        methods = new List<CharacterMethod>();
-    }
-
-    public Character(string name, string description, List<Character> ancestors)
+    public Character(string name, string description, List<Character> parents)
     {
         this.name = name;
         this.description = description;
+
+        this.parents = new List<Character>();
+        this.childrens = new List<Character>();
         this.attributes = new List<CharacterAttribute>();
         this.methods = new List<CharacterMethod>();
-        this.childrens = new List<Character>();
 
+        this.depth = 0;
 
-
-
-        Debug.Log("need help here motherfisher");
-        this.ancestors = ancestors;
-        PreDetails();
-        SetChildrens();
+        PreDetails(parents);
     }
 
     public void InitializeCharacter(Character character)
@@ -51,19 +43,20 @@ public class Character : MonoBehaviour
         this.attributes = character.attributes;
         this.methods = character.methods;
         this.childrens = character.childrens;
-        this.ancestors = character.ancestors;
+        this.parents = character.parents;
         this.depth = character.depth;
     }
 
-
-    private void PreDetails()
+    private void PreDetails(List<Character> parents)
     {
-        foreach (Character character in this.ancestors)
+        for (int i = 0; i < parents.Count; i++)
         {
+            Character character = parents[i];
             if (character.attributes != null)
             {
-                foreach (CharacterAttribute attribute in character.attributes)
+                for (int j = 0; j < character.attributes.Count; j++)
                 {
+                    CharacterAttribute attribute = character.attributes[j];
                     CharacterAttribute newAttribute = new CharacterAttribute(attribute.name, attribute.description, attribute.accessModifier);
                     if (!(attribute.accessModifier == AccessModifier.Private))
                     {
@@ -77,8 +70,9 @@ public class Character : MonoBehaviour
 
             if (character.methods != null)
             {
-                foreach (CharacterMethod method in character.methods)
+                for (int j = 0; j < character.methods.Count; j++)
                 {
+                    CharacterMethod method = character.methods[j];
                     CharacterMethod newMethod = new CharacterMethod(method.name, method.description, method.accessModifier);
                     if (!(method.accessModifier == AccessModifier.Private))
                     {
@@ -89,16 +83,8 @@ public class Character : MonoBehaviour
                     }
                 }
             }
-            this.depth = Math.Max(this.depth, character.depth + 1);
-        }
-    }
 
-    private void SetChildrens()
-    {
-        foreach (Character character in this.ancestors)
-        {
-            Debug.Log("zaaaaaaaaaa");
-            character.childrens.Add(this);
+            this.depth = Math.Max(this.depth, character.depth + 1);
         }
     }
 }
