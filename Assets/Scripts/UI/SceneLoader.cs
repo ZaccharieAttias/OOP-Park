848e8d3ac57f0cd16d6c5d0c78fa2d1a45f83f91
Nitzan <nitzantomer1998@gmail.com>
2023-12-28 11:10:02 +0200

    Updating Structure

--- Assets/Scripts/UI/SceneLoader.cs
+++ Assets/Scripts/UI/SceneLoader.cs
@@ -3,53 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    public void Presentation()
-    {
-        SceneManager.LoadScene("Presentation");
-    }
-
-    public void Playground()
-    {
-        SceneManager.LoadScene("Playground");
-    }
-
-    public void LocalPark()
-    {
-        SceneManager.LoadScene("LocalPark");
-    }
-
-    public void OnlinePark()
-    {
-        SceneManager.LoadScene("OnlinePark");
-    }
-
-    public void Progression()
-    {
-        SceneManager.LoadScene("Progression");
-    }
-
-    public void Credits()
-    {
-        SceneManager.LoadScene("Credits");
-    }
-
-    public void Quit()
-    {
-        Application.Quit();
-    }
-
-    public void Tutorial()
-    {
-        SceneManager.LoadScene("Tutorial");
-    }
-
-    public void Hierarchy()
-    {
-        SceneManager.LoadScene("Hierarchy");
-    }
-
-    public void Polymorphism()
-    {
-        SceneManager.LoadScene("Polymorphism");
-    }
+    public void Presentation() { SceneManager.LoadScene("Presentation"); }
+    public void Playground() { SceneManager.LoadScene("Playground"); }
+    public void LocalPark() { SceneManager.LoadScene("LocalPark"); }
+    public void OnlinePark() { SceneManager.LoadScene("OnlinePark"); }
+    public void Progression() { SceneManager.LoadScene("Progression"); }
+    public void Credits() { SceneManager.LoadScene("Credits"); }
+    public void Tutorial() { SceneManager.LoadScene("Tutorial"); }
+    public void Hierarchy() { SceneManager.LoadScene("Hierarchy"); }
+    public void Polymorphism() { SceneManager.LoadScene("Polymorphism"); }
+    public void Quit() { Application.Quit(); }
 }
