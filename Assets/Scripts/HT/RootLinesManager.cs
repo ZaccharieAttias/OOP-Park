c773ce70bdc7397a0298d87455d064ef67250a25
Zaccharie Attias <zaccharieattias@gmail.com>
2023-12-29 15:16:58 +0100

    Update RootLinesManager.cs

--- Assets/Scripts/HT/RootLinesManager.cs
+++ Assets/Scripts/HT/RootLinesManager.cs
@@ -1,18 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using UnityEngine.EventSystems;
 
 public class RootLinesManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private List<Transform> _linePoints;
+    [SerializeField] private LinesController _linesController;
+    [SerializeField] private List<GameObject> _transformCharacter;
+
+    public void Start()
     {
-        
+        _linePoints = new List<Transform>();
+        _linesController = GameObject.Find("Canvas/HTMenu/Menu/Characters/Tree/Buttons").GetComponent<LinesController>();
+        _transformCharacter = _linesController.GetLinePoints();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetUp(GameObject point)
     {
+        Debug.Log("SetUp");
+        _linePoints.Add(point.transform);
+        Character character = point.GetComponent<CharacterDetails>().GetCurrentCharacter();     
+        if (character.parents.Count == 0) 
+        {
+            Debug.Log("iciciciici");
+            if (character.childrens.Count == 0)
+            {
+                Debug.Log("child 0");
+                return;
+            }
+            SetUp(_transformCharacter.Find(item => item.GetComponent<CharacterDetails>().GetCurrentCharacter().name == character.name).gameObject);
+            return;
+        }
+        if (character.childrens.Count == 0)
+        {  
+            _linePoints.Add(point.transform);
+            Debug.Log("fffff" + _linePoints.Count);
+            _linesController.SetUpLine(_linePoints);
+            _linePoints.Clear();
+            return;
+        }
         
+        for (int i = 0; i < character.childrens.Count; i++)
+        {
+            _linePoints.Add(_transformCharacter.Find(item => item.GetComponent<CharacterDetails>().GetCurrentCharacter().name == character.childrens[i].name).transform);
+            SetUp(_transformCharacter.Find(item => item.GetComponent<CharacterDetails>().GetCurrentCharacter().name == character.childrens[i].name));
+        }
     }
 }
