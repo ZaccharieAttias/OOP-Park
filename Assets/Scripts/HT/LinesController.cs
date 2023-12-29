7b36556da610e13719fbfc776ea58843c7904b4e
Zaccharie Attias <zaccharieattias@gmail.com>
2023-12-29 15:17:02 +0100

    Update LinesController.cs

--- Assets/Scripts/HT/LinesController.cs
+++ Assets/Scripts/HT/LinesController.cs
@@ -1,11 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+using System;
+using UnityEngine.EventSystems;
 
 public class LinesController : MonoBehaviour
 {
     private LineRenderer _lineRenderer;
-    private List<Transform> _linePoints;
+    public List<Transform> _linePoints;
 
     private void Start()
     {
@@ -13,18 +19,32 @@ public class LinesController : MonoBehaviour
         _linePoints = new List<Transform>();
     }
 
-    public void SetUpLine(List<Transform> linePoints)
-    {
-        _linePoints = linePoints;
-        _lineRenderer.positionCount = _linePoints.Count;
-        DrawLines();
-    }
-
-    private void DrawLines()
+    public void Update()
     {
         for (int i = 0; i < _linePoints.Count; i++)
         {
+            Debug.Log("iiiiiiiiiiiiiiiiiiii" + i);
             _lineRenderer.SetPosition(i, _linePoints[i].position);
         }
+    }
+
+    public void SetUpLine(List<Transform> linePoints)
+    {
+        _linePoints = linePoints;
+        Debug.Log("SetUpLine" + _linePoints.Count);
+        _lineRenderer.positionCount = _linePoints.Count;
+    }
+
+    public List<GameObject> GetLinePoints()
+    {
+        List<GameObject> transformCharacter = new List<GameObject>();
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).tag == "Character")
+            {
+                transformCharacter.Add(transform.GetChild(i).gameObject);
+            }
+        }
+        return transformCharacter;
     }
 }
