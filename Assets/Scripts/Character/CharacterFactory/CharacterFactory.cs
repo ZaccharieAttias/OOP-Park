06750d080a33515c94a21171c3cb90a02d7c17f9
Nitzan <nitzantomer1998@gmail.com>
2024-01-06 17:50:35 +0200

    Updating Character Factory Script

--- Assets/Scripts/Character/CharacterFactory/CharacterFactory.cs
+++ Assets/Scripts/Character/CharacterFactory/CharacterFactory.cs
@@ -7,17 +7,15 @@ using UnityEngine.UI;
 
 public class CharacterFactory : MonoBehaviour
 {
-    public GameObject CharacterObjectPrefab { get; set; }
+    public ExecuteFactory ExecuteFactory { get; set; }
 
     public GameObject AddButton { get; set; }
-    public GameObject ConfirmButton { get; set; }
     public GameObject CancelButton { get; set; }
+    public GameObject ConfirmButton { get; set; }
 
     public List<GameObject> CharacterObjects { get; set; }
     public List<GameObject> DuplicateCharacterObjects { get; set; }
     public List<Character> SelectedCharacterObjects { get; set; }
-
-    public CharacterManager CharacterManager { get; set; }
 
     public int ParentsLimit { get; set; }
 
@@ -41,22 +39,23 @@ public class CharacterFactory : MonoBehaviour
 
     private void InitializeProperties()
     {
-        CharacterObjectPrefab = Resources.Load<GameObject>("Prefabs/Buttons/Character");
+        GameObject pureButtonPrefab = Resources.Load<GameObject>("Prefabs/Buttons/Pure");
 
-        AddButton = Instantiate(Resources.Load<GameObject>("Prefabs/Buttons/Button"));
+        AddButton = Instantiate(pureButtonPrefab, transform.parent);
         AddButton.AddComponent<AddFactory>();
 
-        ConfirmButton = Instantiate(Resources.Load<GameObject>("Prefabs/Buttons/Button"));
+        CancelButton = Instantiate(pureButtonPrefab, transform.parent);
+        CancelButton.AddComponent<CancelFactory>();
+
+        ConfirmButton = Instantiate(pureButtonPrefab, transform.parent);
         ConfirmButton.AddComponent<ConfirmFactory>();
 
-        CancelButton = Instantiate(Resources.Load<GameObject>("Prefabs/Buttons/Button"));
-        CancelButton.AddComponent<CancelFactory>();
+        GameObject executeFactoryObject = new GameObject("ExecuteFactory", typeof(ExecuteFactory));
+        ExecuteFactory = executeFactoryObject.GetComponent<ExecuteFactory>();
 
         CharacterObjects = new List<GameObject>();
         DuplicateCharacterObjects = new List<GameObject>();
         SelectedCharacterObjects = new List<Character>();
-
-        CharacterManager = GameObject.Find("Player").GetComponent<CharacterManager>();
 
         ParentsLimit = RestrictionManager.Instance.AllowMultipleInheritance ? 5 : 1;
     }
@@ -64,24 +63,34 @@ public class CharacterFactory : MonoBehaviour
     public void InitializeFactory()
     {
         AddButton.GetComponent<Button>().interactable = false;
-        ConfirmButton.SetActive(true);
         CancelButton.SetActive(true);
+        ConfirmButton.SetActive(true);
 
         CharacterObjects = GameObject.FindGameObjectsWithTag("CharacterButton").ToList();
         BuildDuplicateCharacterObjects();
         ToggleButtonInteractability(CharacterObjects);
     }
 
+    public void CancelFactory()
+    {
+        CancelButton.GetComponent<Button>().interactable = false;
+        ConfirmButton.GetComponent<Button>().interactable = false;
+
+        SelectedCharacterObjects.Clear();
+
+        UpdatePanelUI();
+    }
+
     public void ConfirmFactory()
     {
-        ConfirmButton.SetActive(false);
         CancelButton.SetActive(false);
+        ConfirmButton.SetActive(false);
 
         AddButton.GetComponent<Button>().interactable = true;
-        ConfirmButton.GetComponent<Button>().interactable = false;
         CancelButton.GetComponent<Button>().interactable = false;
+        ConfirmButton.GetComponent<Button>().interactable = false;
 
-        CharacterManager.AddCharacter(SelectedCharacterObjects);
+        ExecuteFactory.Execute();
 
         DestroyObjectsList(DuplicateCharacterObjects);
         ToggleButtonInteractability(CharacterObjects);
@@ -91,44 +100,26 @@ public class CharacterFactory : MonoBehaviour
         SelectedCharacterObjects.Clear();
     }
 
-    public void CancelFactory()
-    {
-        ConfirmButton.GetComponent<Button>().interactable = false;
-        CancelButton.GetComponent<Button>().interactable = false;
-
-        SelectedCharacterObjects.Clear();
-
-        UpdateCharacterObjectsUI();
-    }
-
     private void CharacterObjectClicked()
     {
         GameObject characterObject = EventSystem.current.currentSelectedGameObject;
-        Character character = characterObject.GetComponent<CharacterDetails>().GetCurrentCharacter();
+        Character character = characterObject.GetComponent<CharacterDetails>().Character;
 
         if (SelectedCharacterObjects.Contains(character)) SelectedCharacterObjects.Remove(character);
         else SelectedCharacterObjects.Add(character);
 
-        if (SelectedCharacterObjects.Count > 0)
-        {
-            ConfirmButton.GetComponent<Button>().interactable = true;
-            CancelButton.GetComponent<Button>().interactable = true;
-        }
-
-        else
-        {
-            ConfirmButton.GetComponent<Button>().interactable = false;
-            CancelButton.GetComponent<Button>().interactable = false;
-        }
-
-        UpdateCharacterObjectsUI();
+        UpdatePanelUI();
     }
 
-    private void UpdateCharacterObjectsUI()
+    private void UpdatePanelUI()
     {
+        bool isSelectedEmpty = SelectedCharacterObjects.Count > 0;
+        CancelButton.GetComponent<Button>().interactable = isSelectedEmpty;
+        ConfirmButton.GetComponent<Button>().interactable = isSelectedEmpty;
+
         foreach (GameObject characterObject in DuplicateCharacterObjects)
         {
-            Character currentCharacter = characterObject.GetComponent<CharacterDetails>().GetCurrentCharacter();
+            Character currentCharacter = characterObject.GetComponent<CharacterDetails>().Character;
 
             bool isSelected = SelectedCharacterObjects.Contains(currentCharacter);
             bool isParentLimitExceeded = SelectedCharacterObjects.Count == ParentsLimit;
@@ -141,14 +132,14 @@ public class CharacterFactory : MonoBehaviour
         {
             foreach (Character parent in character.parents)
             {
-                GameObject parentObject = DuplicateCharacterObjects.Find(obj => obj.GetComponent<CharacterDetails>().GetCurrentCharacter().name == parent.name);
+                GameObject parentObject = DuplicateCharacterObjects.Find(obj => obj.GetComponent<CharacterDetails>().Character.name == parent.name);
                 parentObject.GetComponent<Button>().interactable = false;
                 parentObject.GetComponent<Image>().color = Color.black;
             }
 
             foreach (Character child in character.childrens)
             {
-                GameObject childObject = DuplicateCharacterObjects.Find(obj => obj.GetComponent<CharacterDetails>().GetCurrentCharacter().name == child.name);
+                GameObject childObject = DuplicateCharacterObjects.Find(obj => obj.GetComponent<CharacterDetails>().Character.name == child.name);
                 childObject.GetComponent<Button>().interactable = false;
                 childObject.GetComponent<Image>().color = Color.black;
             }
@@ -162,10 +153,7 @@ public class CharacterFactory : MonoBehaviour
         foreach (GameObject characterObject in CharacterObjects)
         {
             GameObject duplicateCharacterObject = Instantiate(characterObject, characterObject.transform.parent);
-
-            duplicateCharacterObject.GetComponent<RectTransform>().sizeDelta = characterObject.GetComponent<RectTransform>().sizeDelta;
-            duplicateCharacterObject.transform.localScale = new Vector3(1, 1, 1);
-
+            
             Button duplicateCharacterObjectButton = duplicateCharacterObject.GetComponent<Button>();
             duplicateCharacterObjectButton.onClick.RemoveAllListeners();
             duplicateCharacterObjectButton.onClick.AddListener(() => CharacterObjectClicked());
