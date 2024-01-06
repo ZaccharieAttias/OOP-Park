34112a1eeb573d90e8daebcb771bed9da68974ef
Nitzan <nitzantomer1998@gmail.com>
2024-01-06 17:51:03 +0200

    Updating Cancel Factory Script

--- Assets/Scripts/Character/CharacterFactory/CancelFactory.cs
+++ Assets/Scripts/Character/CharacterFactory/CancelFactory.cs
@@ -4,19 +4,14 @@ using UnityEngine.UI;
 
 public class CancelFactory : MonoBehaviour
 {
-    private const string spritePath = "Sprites/Icons/Delete";
-    private const string characterFactoryPath = "Canvas/HTMenu/Menu/Characters/Tree/Buttons/CharacterFactory";
-
-
     public void Start() { InitializeGameObject(); }
 
     private void InitializeGameObject()
     {
-        GameObject characterFactory = GameObject.Find(characterFactoryPath);
+        GameObject characterFactory = GameObject.Find("Canvas/HTMenu/Menu/Characters/Tree/Buttons/CharacterFactory");
 
         name = "CancelFactory";
         
-        transform.SetParent(characterFactory.transform);
         transform.localPosition = new Vector3(-168, -237, 0);
         transform.localScale = new Vector3(1, 1, 1);
 
@@ -24,7 +19,7 @@ public class CancelFactory : MonoBehaviour
         rectTransform.sizeDelta = new Vector2(20, 20);
         
         Image image = GetComponent<Image>();
-        image.sprite = Resources.Load<Sprite>(spritePath);
+        image.sprite = Resources.Load<Sprite>("Sprites/Icons/Delete");
         image.color = Color.white;
         
         Button button = GetComponent<Button>();
