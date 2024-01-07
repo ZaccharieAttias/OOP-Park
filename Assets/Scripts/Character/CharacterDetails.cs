e2889300b4c45a6f197423306ff713b99ab6aa20
Nitzan <nitzantomer1998@gmail.com>
2024-01-07 23:38:30 +0200

    Removing Unneeded Method

--- Assets/Scripts/Character/CharacterDetails.cs
+++ Assets/Scripts/Character/CharacterDetails.cs
@@ -1,16 +1,10 @@
 using UnityEngine;
 
+
 public class CharacterDetails : MonoBehaviour
 {
-    [SerializeField] public Character Character;
+    [SerializeField] public Character Character { get; set;}
 
-    public void InitializeCharacter(Character character)
-    {
-        Character = character;
-    }
 
-    public Character GetCurrentCharacter()
-    {
-        return this.Character;
-    }
+    public void InitializeCharacter(Character character) { Character = character; }
 }
