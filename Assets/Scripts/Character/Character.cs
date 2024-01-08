1598de2284225464aac81c30eee1a0322114e11f
Nitzan <nitzantomer1998@gmail.com>
2024-01-08 13:15:56 +0200

    Modify Methods & Encapsulate CharacterButton Attribute

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -1,28 +1,21 @@
 using System.Collections.Generic;
-using System.Linq;
-using System;
 using UnityEngine;
 
 
-[Serializable]
 public class Character
 {
-    public bool IsOriginal { get; set; }
+    public bool IsOriginal;
 
-    public string Name { get; set; }
-    public string Description { get; set; }
+    public string Name;
+    public string Description;
 
-    public List<Character> Parents;
-    public List<Character> Childrens;
     public List<CharacterAttribute> Attributes;
     public List<CharacterMethod> Methods;
 
-    public int X { get; set; }
-    public int Y { get; set; }
-    public int Mod { get; set; }
-    public int Depth { get; set; }
+    public List<Character> Parents;
+    public List<Character> Childrens;
 
-    public GameObject CharacterButton { get; set; }
+    public CharacterButton CharacterButton;
 
 
     public Character(string name, string description, List<Character> parents, bool isOriginal)
@@ -32,35 +25,13 @@ public class Character
         Name = name;
         Description = description;
 
-        Parents = new List<Character>(parents);
-        Childrens = new List<Character>();
         Attributes = new List<CharacterAttribute>();
         Methods = new List<CharacterMethod>();
 
-        X = 0;
-        Y = 0;
-        Mod = 0;
-        Depth = 0;
+        Parents = new List<Character>(parents);
+        Childrens = new List<Character>();
 
-        CharacterButton = null;
-
-        PreDetails();
-    }
-
-    private void PreDetails()
-    {
-        foreach (Character character in Parents)
-        {
-            foreach (CharacterAttribute attribute in character.Attributes)
-                if (attribute.accessModifier != AccessModifier.Private)
-                    if (Attributes.Any(item => item.name == attribute.name) == false)
-                        Attributes.Add(new CharacterAttribute(attribute.name, attribute.description, attribute.accessModifier));
-
-            foreach (CharacterMethod method in character.Methods)
-                if (method.accessModifier != AccessModifier.Private)
-                    if (Methods.Any(item => item.name == method.name) == false)
-                        Methods.Add(new CharacterMethod(method.name, method.description, method.accessModifier));
-        }
+        CharacterButton = new CharacterButton();
     }
 
     public void InitializeCharacter(Character character)
@@ -70,89 +41,48 @@ public class Character
         Name = character.Name;
         Description = character.Description;
 
-        Parents = character.Parents;
-        Childrens = character.Childrens;
         Attributes = character.Attributes;
         Methods = character.Methods;
 
-        X = character.X;
-        Y = character.Y;
-        Mod = character.Mod;
-        Depth = character.Depth;
+        Parents = character.Parents;
+        Childrens = character.Childrens;
 
         CharacterButton = character.CharacterButton;
     }
 
-    public void Dispose()
-    {
-        // Removing himself from its parnents
-
-        foreach (Character parent in Parents)
-        {
-            parent.Childrens.Remove(this);
-        }
-    }
-
-
+    public bool IsRoot() { return Parents.Count == 0; }
     public bool IsLeaf() { return Childrens.Count == 0; }
 
-    public bool IsLeftMost()
-    {
-        if (Parents.Count == 0) return true;
+    public bool IsLeftMost() { return IsRoot() || Parents[0].Childrens[0] == this; }
+    public bool IsRightMost() { return IsRoot() || Parents[0].Childrens[^1] == this; }
 
-        return Parents[0].Childrens[0] == this;
-    }
+    public Character GetLeftMostSibling() { return IsRoot() ? null : Parents[0].Childrens[0]; }
+    public Character GetLeftMostChild() { return IsLeaf() ? null : Childrens[0]; }
 
-    public bool IsRightMost()
-    {
-        if (Parents.Count == 0) return true;
-
-        return Parents[0].Childrens[^1] == this;
-    }
+    public Character GetRightMostSibling() { return IsRoot() ? null : Parents[0].Childrens[^1]; }
+    public Character GetRightMostChild() { return IsLeaf() ? null : Childrens[^1]; }
 
     public Character GetPreviousSibling()
     {
-        if (Parents.Count == 0 || IsLeftMost()) return null;
+        if (IsLeftMost()) return null;
 
         return Parents[0].Childrens[Parents[0].Childrens.IndexOf(this) - 1];
     }
-
     public Character GetNextSibling()
     {
-        if (Parents.Count == 0 || IsRightMost()) return null;
+        if (IsRightMost()) return null;
 
         return Parents[0].Childrens[Parents[0].Childrens.IndexOf(this) + 1];
     }
 
-    public Character GetLeftMostSibling()
-    {
-        if (Parents.Count == 0) return null;
-        if (IsLeftMost()) return this;
-
-        return Parents[0].Childrens[0];
-    }
-
-    public Character GetLeftMostChild()
-    {
-        if (Childrens.Count == 0) return null;
-
-        return Childrens[0];
-    }
-
-    public Character GetRightMostChild()
-    {
-        if (Childrens.Count == 0) return null;
-
-        return Childrens[^1];
-    }
-
     public void SetTransformPositionX(float x)
     {
-        CharacterButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(x, CharacterButton.GetComponent<RectTransform>().anchoredPosition.y);
+        RectTransform rectTransform = CharacterButton.Button.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(x, rectTransform.anchoredPosition.y);
     }
-
     public void SetTransformPositionY(float y)
     {
-        CharacterButton.GetComponent<RectTransform>().anchoredPosition = new Vector2(CharacterButton.GetComponent<RectTransform>().anchoredPosition.x, y);
+        RectTransform rectTransform = CharacterButton.Button.GetComponent<RectTransform>();
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, y);
     }
 }
