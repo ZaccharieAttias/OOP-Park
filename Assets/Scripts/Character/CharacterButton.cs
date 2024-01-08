2211b4d4f536bd6eabda36b268e07b98975bb3d1
Nitzan <nitzantomer1998@gmail.com>
2024-01-08 13:19:40 +0200

    Character GameObject Class

---
+++ Assets/Scripts/Character/CharacterButton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+
+public class CharacterButton
+{
+    public GameObject Button;
+    
+    public int X;
+    public int Y;
+    public int Mod;
+    public int Depth;
+
+
+    public CharacterButton()
+    {
+        Button = null;
+        
+        X = 0;
+        Y = 0;
+        Mod = 0;
+        Depth = 0;
+    }
+}
