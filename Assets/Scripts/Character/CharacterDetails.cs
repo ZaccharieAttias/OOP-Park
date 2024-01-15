a91b5b9c0d07d44fde54d7c0f922a2d1de90c1a4
Zaccharie Attias <zaccharieattias@gmail.com>
2024-01-15 15:38:01 +0200

    Update CharacterDetails.cs

--- Assets/Scripts/Character/CharacterDetails.cs
+++ Assets/Scripts/Character/CharacterDetails.cs
@@ -1,10 +1,9 @@
 using UnityEngine;
 
-
+[System.Serializable]
 public class CharacterDetails : MonoBehaviour
 {
     public Character Character;
-
 
     public void InitializeCharacter(Character character) { Character = character; }
 }
