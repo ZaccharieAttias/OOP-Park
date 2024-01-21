32aca34605c165a152398ede8676a76b775e5e89
Nitzan <nitzantomer1998@gmail.com>
2024-01-21 21:09:25 +0200

    Update Structre

--- Assets/Scripts/Abilities/PowerUpEffect.cs
+++ Assets/Scripts/Abilities/PowerUpEffect.cs
@@ -1,9 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public abstract class PowerUpEffect : ScriptableObject
 {
-    public abstract void ActivatePower(GameObject player);
+    public abstract void ActivatePower(GameObject player, float value);
     public abstract void DeactivatePower(GameObject player);
 }
