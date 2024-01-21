3833914c8c4bd18ac81bcc587d5c72e6f2929154
Nitzan <nitzantomer1998@gmail.com>
2024-01-21 21:23:34 +0200

    Adding Value Field

--- Assets/Scripts/Character/CharacterAttribute.cs
+++ Assets/Scripts/Character/CharacterAttribute.cs
@@ -1,15 +1,19 @@
 [System.Serializable]
 public class CharacterAttribute
 {
-    public string name;
-    public string description;
-    public AccessModifier accessModifier;
+    public string Name;
+    public string Description;
+    public float Value;
+
+    public AccessModifier AccessModifier;
 
     
-    public CharacterAttribute(string name, string description, AccessModifier accessModifier)
+    public CharacterAttribute(string name, string description, float value, AccessModifier accessModifier)
     {
-        this.name = name;
-        this.description = description;
-        this.accessModifier = accessModifier;
+        Name = name;
+        Description = description;
+        Value = value;
+        
+        AccessModifier = accessModifier;
     }
 }
