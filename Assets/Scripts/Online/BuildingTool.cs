b6d7c367c44dd3e31d187b31c6d80400f8484dbb
Zaccharie Attias <zaccharieattias@gmail.com>
2024-01-28 17:02:44 +0200

    update BuidingTool

--- Assets/Scripts/Online/BuildingTool.cs
+++ Assets/Scripts/Online/BuildingTool.cs
@@ -1,27 +1,29 @@
-// using UnityEngine;
+using UnityEngine;
 
-// enum ToolType {
-//     None,
-//     Eraser
-// }
+enum ToolType {
+    None,
+    Eraser
+}
 
-// [CreateAssetMenu(fileName = "Tool", menuName = "LevelBuilding/Create Tool")]
-// public class BuildingTool : BuildingObjectBase {
-//     [SerializeField] private ToolType toolType;
-//     ToolController tc;
+[CreateAssetMenu(fileName = "Tool", menuName = "LevelBuilding/Create Tool")]
+public class BuildingTool : BuildingObjectBase {
+    [SerializeField] private ToolType toolType;
+    ToolController tc;
 
-//     void Awake() {
-//         tc = ToolController.GetInstance();
-//     }
+    void Awake() {
+        tc = ToolController.GetInstance();
+    }
 
-//     public void Use(Vector3Int position) {
-//         switch (toolType) {
-//             case ToolType.Eraser:
-//                 tc.Eraser(position);
-//                 break;
-//             default:
-//                 Debug.LogError("Tool Type not set");
-//                 break;
-//         }
-//     }
-// }
+    public void Use(Vector3Int position) {
+        if( tc ==null)
+            tc = ToolController.GetInstance();
+        switch (toolType) {
+            case ToolType.Eraser:
+                tc.Eraser(position);
+                break;
+            default:
+                Debug.LogError("Tool Type not set");
+                break;
+        }
+    }
+}
