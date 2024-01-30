23fc2e65c74d86532ac48a8b277aa4b3bb4389cd
Zaccharie Attias <zaccharieattias@gmail.com>
2024-01-30 13:50:58 +0200

    Update BuildingCreator.cs

--- Assets/Scripts/Online/BuildingCreator.cs
+++ Assets/Scripts/Online/BuildingCreator.cs
@@ -9,15 +9,14 @@ using System.Linq;
 
 public class BuildingCreator : Singleton<BuildingCreator> {
     [SerializeField]
-    Tilemap previewMap,
-    defaultMap;
+    Tilemap defaultMap, toolPreviewMap;
 
     // if one of those maps contains a tile at the position, placing is not allowed
     [SerializeField] List<Tilemap> forbidPlacingWithMaps;
 
     PlayerInput playerInput;
-
     BuildingHistory buildingHistory;
+    PreviewHandler previewHandler = new PreviewHandler();
 
     TileBase tileBase;
     BuildingObjectBase selectedObj;
@@ -65,12 +64,17 @@ public class BuildingCreator : Singleton<BuildingCreator> {
         playerInput.Gameplay.MouseRightClick.performed -= OnRightClick;
     }
 
+    /// <section>
+    /// GETTER & SETTER
+    /// </section>
+
     private BuildingObjectBase SelectedObj {
         set {
             selectedObj = value;
 
             tileBase = selectedObj != null ? selectedObj.TileBase : null;
 
+            previewHandler.UpdateTile(tilemap, tileBase);
             UpdatePreview();
         }
     }
@@ -81,28 +85,41 @@ public class BuildingCreator : Singleton<BuildingCreator> {
                 return selectedObj.Category.Tilemap;
             }
 
+            if (selectedObj != null && selectedObj is BuildingTool) {
+                return toolPreviewMap;
+            }
+
             return defaultMap;
         }
     }
+
+    /// <section>
+    /// UPDATE
+    /// </section>
 
     private void Update() {
         // if something is selected - show preview
         if (selectedObj != null) {
             Vector3 pos = _camera.ScreenToWorldPoint(mousePos);
-            Vector3Int gridPos = previewMap.WorldToCell(pos);
+            Vector3Int gridPos = tilemap.WorldToCell(pos);
 
             if (gridPos != currentGridPosition) {
                 lastGridPosition = currentGridPosition;
                 currentGridPosition = gridPos;
 
-                UpdatePreview();
 
                 if (holdActive) {
                     HandleDrawing();
+                } else {
+                    UpdatePreview();
                 }
             }
         }
     }
+
+    /// <section>
+    /// INPUT HANDLING
+    /// </section>
 
     private void OnMouseMove(InputAction.CallbackContext ctx) {
         mousePos = ctx.ReadValue<Vector2>();
@@ -123,54 +140,52 @@ public class BuildingCreator : Singleton<BuildingCreator> {
                     HandleDrawRelease();
                 }
             }
+        } else {
+            previewHandler.ResetPreview();
+            holdActive = false;
         }
     }
 
     private void OnRightClick(InputAction.CallbackContext ctx) {
+        previewHandler.ResetPreview();
+        holdActive = false;
         SelectedObj = null;
     }
+
+    /// <section>
+    /// PUBLIC METHODS
+    /// </section>
 
     public void ObjectSelected(BuildingObjectBase obj) {
         SelectedObj = obj;
     }
 
+    /// <section>
+    /// DRAW HANDLING
+    /// </section>
+
     private void UpdatePreview() {
-        // Remove old tile if existing
-        previewMap.SetTile(lastGridPosition, null);
-
-        if (!IsForbidden(currentGridPosition)) {
-            // Set current tile to current mouse positions tile
-            previewMap.SetTile(currentGridPosition, tileBase);
-        }
-    }
-
-    private bool IsForbidden(Vector3Int pos) {
-        if (selectedObj == null) return false;
-
-        List<BuildingCategory> restrictedCategories = selectedObj.PlacementRestrictions;
-        // get the according tilemaps for each category
-        List<Tilemap> restrictedMaps = restrictedCategories.ConvertAll(category => category.Tilemap);
-
-        // merge both lists together
-        List<Tilemap> allMaps = forbidPlacingWithMaps.Concat(restrictedMaps).ToList();
-
-        return allMaps.Any(map => {
-            return map.HasTile(pos);
-        });
+        previewHandler.ResetPreview(lastGridPosition);
+        previewHandler.SetPreview(currentGridPosition, IsForbidden(currentGridPosition));
     }
 
     private void HandleDrawing() {
         if (selectedObj != null) {
             switch (selectedObj.PlaceType) {
                 case PlaceType.Line:
-                    LineRenderer();
+                    bounds = DrawRenderer.LineRenderer(holdStartPosition, currentGridPosition);
                     break;
                 case PlaceType.Rectangle:
-                    RectangleRenderer();
+                    bounds = DrawRenderer.RectangleRenderer(holdStartPosition, currentGridPosition);
+                    break;
+                default:
+                    bounds = DrawRenderer.SingleRenderer(currentGridPosition);
                     break;
             }
-        }
 
+            // draw bounds for preview map
+            DrawBounds(tilemap, true);
+        }
     }
 
     private void HandleDrawRelease() {
@@ -179,7 +194,6 @@ public class BuildingCreator : Singleton<BuildingCreator> {
                 case PlaceType.Line:
                 case PlaceType.Rectangle:
                     DrawBounds(tilemap);
-                    previewMap.ClearAllTiles();
                     break;
                 case PlaceType.Single:
                 default:
@@ -189,54 +203,21 @@ public class BuildingCreator : Singleton<BuildingCreator> {
         }
     }
 
-    private void RectangleRenderer() {
-        //  Render Preview on UI Map, draw real one on Release
-
-        previewMap.ClearAllTiles();
-
-        bounds.xMin = currentGridPosition.x < holdStartPosition.x ? currentGridPosition.x : holdStartPosition.x;
-        bounds.xMax = currentGridPosition.x > holdStartPosition.x ? currentGridPosition.x : holdStartPosition.x;
-        bounds.yMin = currentGridPosition.y < holdStartPosition.y ? currentGridPosition.y : holdStartPosition.y;
-        bounds.yMax = currentGridPosition.y > holdStartPosition.y ? currentGridPosition.y : holdStartPosition.y;
-
-        DrawBounds(previewMap);
-    }
-
-    private void LineRenderer() {
-        //  Render Preview on UI Map, draw real one on Release
-
-        previewMap.ClearAllTiles();
-
-        float diffX = Mathf.Abs(currentGridPosition.x - holdStartPosition.x);
-        float diffY = Mathf.Abs(currentGridPosition.y - holdStartPosition.y);
-
-        bool lineIsHorizontal = diffX >= diffY;
-
-        if (lineIsHorizontal) {
-            bounds.xMin = currentGridPosition.x < holdStartPosition.x ? currentGridPosition.x : holdStartPosition.x;
-            bounds.xMax = currentGridPosition.x > holdStartPosition.x ? currentGridPosition.x : holdStartPosition.x;
-            bounds.yMin = holdStartPosition.y;
-            bounds.yMax = holdStartPosition.y;
-        } else {
-            bounds.xMin = holdStartPosition.x;
-            bounds.xMax = holdStartPosition.x;
-            bounds.yMin = currentGridPosition.y < holdStartPosition.y ? currentGridPosition.y : holdStartPosition.y;
-            bounds.yMax = currentGridPosition.y > holdStartPosition.y ? currentGridPosition.y : holdStartPosition.y;
-        }
-
-        DrawBounds(previewMap);
-    }
-
-    private void DrawBounds(Tilemap map) {
-        // Draws bounds on given map
-
+    private void DrawBounds(Tilemap map, bool isPreview = false) {
         List<Vector3Int> positions = new List<Vector3Int>();
+        List<bool> isForbidden = new List<bool>();
         for (int x = bounds.xMin; x <= bounds.xMax; x++) {
             for (int y = bounds.yMin; y <= bounds.yMax; y++) {
                 positions.Add(new Vector3Int(x, y, 0));
+                isForbidden.Add(IsForbidden(new Vector3Int(x, y, 0)));
             }
         }
-        DrawItem(map, positions.ToArray(), tileBase);
+
+        if (isPreview) {
+            previewHandler.SetPreview(positions.ToArray(), isForbidden.ToArray());
+        } else {
+            DrawItem(map, positions.ToArray(), tileBase);
+        }
     }
 
     private void DrawItem(Tilemap map, Vector3Int position, TileBase tileBase) {
@@ -246,8 +227,10 @@ public class BuildingCreator : Singleton<BuildingCreator> {
 
     private void DrawItem(Tilemap map, Vector3Int[] positions, TileBase tileBase) {
 
-        if (map != previewMap && selectedObj.GetType() == typeof(BuildingTool)) {
+        if (selectedObj.GetType() == typeof(BuildingTool)) {
             // it is a tool
+            previewHandler.ResetPreview();
+
             BuildingTool tool = (BuildingTool)selectedObj;
 
             tool.Use(positions, out BuildingHistoryStep historyStep);
@@ -262,7 +245,7 @@ public class BuildingCreator : Singleton<BuildingCreator> {
             TileBase[] newTiles = new TileBase[positions.Length];
 
             for (int i = 0; i < positions.Length; i++) {
-                previousTiles[i] = map.GetTile(positions[i]);
+                previousTiles[i] = previewHandler.GetPreviousTile(positions[i]);
 
                 if (!IsForbidden(positions[i])) {
                     newTiles[i] = tileBase;
@@ -273,13 +256,30 @@ public class BuildingCreator : Singleton<BuildingCreator> {
                 }
             }
 
-            if (map != previewMap) {
-                buildingHistory.Add(new BuildingHistoryStep(map, previousTiles, newTiles, positions));
-            }
+            buildingHistory.Add(new BuildingHistoryStep(map, previousTiles, newTiles, positions));
 
-
+            previewHandler.Clear();
         }
 
+        previewHandler.SetPreview(currentGridPosition, IsForbidden(currentGridPosition));
     }
 
+    private bool IsForbidden(Vector3Int pos) {
+        if (selectedObj == null) return false;
+
+        List<BuildingCategory> restrictedCategories = selectedObj.PlacementRestrictions;
+        // get the according tilemaps for each category
+        List<Tilemap> restrictedMaps = restrictedCategories.ConvertAll(category => category.Tilemap);
+
+        // merge both lists together
+        List<Tilemap> allMaps = forbidPlacingWithMaps.Concat(restrictedMaps).ToList();
+
+        return allMaps.Any(map => {
+            if (map.HasTile(pos)) {
+                return map.GetTile(pos) != tileBase;
+            }
+
+            return false;
+        });
+    }
 }
