109c10679453cb0e670e919dc48b8c689e8fe812
Nitzan <nitzantomer1998@gmail.com>
2024-02-02 15:18:31 +0200

    Updating Name, LevelCreation --> SeedCollections

--- Assets/Scripts/Helpers/SeedCollections.cs
+++ Assets/Scripts/Helpers/SeedCollections.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-public class LevelCreation : MonoBehaviour
+public class SeedCollections : MonoBehaviour
 {
     AttributesPopupManager AttributesPopupManager;
     MethodsPopupManager MethodsPopupManager;
