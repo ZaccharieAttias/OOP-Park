a0987686d2e843a23adf7320ad19394e0dfb8bc7
Nitzan <nitzantomer1998@gmail.com>
2024-02-02 16:17:44 +0200

    Transfer ChecKPoint Location

--- Assets/Scripts/Gameplay/Elements/Checkpoint.cs
+++ Assets/Scripts/Gameplay/Elements/Checkpoint.cs
@@ -1,26 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
 
 public class Checkpoint : MonoBehaviour
 {
-    GameController gameController; 
+    public GameController gameController; 
     public Transform respawnPoint;
 
-    SpriteRenderer spriteRenderer;
+    public SpriteRenderer spriteRenderer;
     public Sprite passive, active;
-    Collider2D col;
+    public Collider2D col;
 
-    void Awake()
+    public void Start() { InitializeProperties(); }
+    private void InitializeProperties()
     {
         gameController = GameObject.FindGameObjectsWithTag("Player")[0].GetComponent<GameController>();
         spriteRenderer = GetComponent<SpriteRenderer>();
-        col = GetComponent<Collider2D>();  
+        col = GetComponent<Collider2D>();
     }
-
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
             gameController.UpdateCheckpoint(respawnPoint.position);
             spriteRenderer.sprite = active;
