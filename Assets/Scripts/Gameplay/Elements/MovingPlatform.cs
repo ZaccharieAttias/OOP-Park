3beb68bdb52ef0ebad043f6f6ba8a83af2c5b2bf
Nitzan <nitzantomer1998@gmail.com>
2024-02-03 09:14:04 +0200

    Updating Script & Scene using Insepctor

--- Assets/Scripts/Gameplay/Elements/MovingPlatform.cs
+++ Assets/Scripts/Gameplay/Elements/MovingPlatform.cs
@@ -5,26 +5,20 @@ public class MovingPlatform : MonoBehaviour
 {
     public Transform PointA;
     public Transform PointB;
-    public Vector2 Position;
+    public Vector2 PlatformPosition;
 
     public float Speed;
 
-    public void Start() { InitializeProperties(); }
-    private void InitializeProperties()
-    {
-        PointA = GameObject.Find("Grid/MovingPlatforms/PointA").GetComponent<Transform>();
-        PointB = GameObject.Find("Grid/MovingPlatforms/PointB").GetComponent<Transform>();
-        Position = PointA.position;
 
-        Speed = 2f;
-    }
+    public void Start() { InitializeProperties(); }
+    private void InitializeProperties() { PlatformPosition = PointA.position; }
 
     public void Update()
     {
-        if (Vector2.Distance(transform.position, PointA.position) < 0.5f) Position = PointB.position;
-        if (Vector2.Distance(transform.position, PointB.position) < 0.5f) Position = PointA.position;
+        if (Vector2.Distance(transform.position, PointA.position) < 0.5f) PlatformPosition = PointB.position;
+        if (Vector2.Distance(transform.position, PointB.position) < 0.5f) PlatformPosition = PointA.position;
         
-        transform.position = Vector3.MoveTowards(transform.position, Position, Speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, PlatformPosition, Speed * Time.deltaTime);
     }
 
     public void OnTriggerEnter2D(Collider2D collision) { collision.transform.parent = collision.CompareTag("Player") ? transform : null; }
