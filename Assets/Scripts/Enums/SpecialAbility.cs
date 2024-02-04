079974fb594425125ffd04705df9266cf966971d
Zaccharie Attias <zaccharieattias@gmail.com>
2024-02-04 17:01:28 +0200

    Update SpecialAbility.cs

--- Assets/Scripts/Enums/SpecialAbility.cs
+++ Assets/Scripts/Enums/SpecialAbility.cs
@@ -1,1 +1,1 @@
-public enum SpecialAbility { General, Automatic, Jump, Gravity, Speed, DoubleJump, WeakGravity, FastSpeed }
+public enum SpecialAbility { General, Automatic, Jump, Gravity, Speed, DoubleJump, WeakGravity, FastSpeed, FireballShoot }
