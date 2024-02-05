10f8f226f6cf6734346d17b1a4194a6eace18910
Nitzan <nitzantomer1998@gmail.com>
2024-02-05 14:12:54 +0200

    Right Click Script for Displaying Button Description

---
+++ Assets/Scripts/UI/Buttons/RightClickButton.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class RightClickButton : MonoBehaviour, IPointerClickHandler
+{
+    public CharacterManager CharacterManager;
+
+
+    public void Start() { InitializeProperties(); }
+    private void InitializeProperties() { CharacterManager = GameObject.Find("Player").GetComponent<CharacterManager>(); }
+    
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right)
+        {
+            string gameObjectDescription = null;
+            
+            gameObjectDescription ??= CharacterManager.CurrentCharacter.Attributes.Find(item => item.Name == gameObject.name)?.Description;
+            gameObjectDescription ??= CharacterManager.CurrentCharacter.Methods.Find(item => item.Name == gameObject.name)?.Description;
+            gameObjectDescription ??= CharacterManager.CurrentCharacter.SpecialAbility.Name == gameObject.name ? CharacterManager.CurrentCharacter.SpecialAbility.Description : null;
+
+            CharacterManager.DescriptionText.text = gameObjectDescription;
+        }
+    }
+}
