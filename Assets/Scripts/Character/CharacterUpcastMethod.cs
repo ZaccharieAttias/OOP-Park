f514f3ebca991d336b5178fbb891255e3701a3d3
Nitzan <nitzantomer1998@gmail.com>
2024-02-08 15:14:57 +0200

    Improve Efficiency

--- Assets/Scripts/Character/CharacterUpcastMethod.cs
+++ Assets/Scripts/Character/CharacterUpcastMethod.cs
@@ -1,6 +1,4 @@
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 
 [System.Serializable]
@@ -8,49 +6,18 @@ public class CharacterUpcastMethod
 {
     public Character Character;
     public CharacterMethod CharacterMethod;
-    public float Quantity;
+    public float Amount;
 
-    public CharacterManager CharacterManager;
-    public GameObject ContentPanel;
-    public TextMeshProUGUI QuantityDescriptionText;
+    public UpcastTrackerManager UpcastTrackerManager;
 
-    public void Start()
-    {
-        CharacterManager = GameObject.Find("Player").GetComponent<CharacterManager>();
 
-        ContentPanel = GameObject.Find("Canvas/Popups/SpecialAbilityCoolDown");
-        QuantityDescriptionText = GameObject.Find("Canvas/Popups/SpecialAbilityCoolDown/Description").GetComponent<TextMeshProUGUI>();
-
-        Button activateGameplayButton = GameObject.Find("Canvas/HTMenu/Menu/SwapScreen").GetComponent<Button>();
-        activateGameplayButton.onClick.AddListener(() => ToggleOn());
-
-        Button activateMenuButton = GameObject.Find("Canvas/GameplayScreen/SwapScreen").GetComponent<Button>();
-        activateMenuButton.onClick.AddListener(() => ToggleOff());
-    }
-
-    public CharacterUpcastMethod(Character character, CharacterMethod characterMethod, float quantity)
+    public CharacterUpcastMethod(Character character, CharacterMethod characterMethod, float amount)
     {
         Character = character;
         CharacterMethod = characterMethod;
-        Quantity = quantity;
+        Amount = amount;
 
-        Start();
+        UpcastTrackerManager = GameObject.Find("Canvas/Popups").GetComponent<UpcastTrackerManager>();
+        UpcastTrackerManager.AmountDescriptionText.text = ((int)Amount).ToString();
     }
-
-    public void UpdateUpcast(float quantity) 
-    { 
-        Quantity -= quantity;
-        
-        if (Quantity <= 0) 
-        {
-            Character.UpcastMethod = null;
-            GameObject.Find("Player").GetComponent<CharacterManager>().DisplayCharacter(Character);
-            ToggleOff();
-        }
-
-        else { QuantityDescriptionText.text = ((int)Quantity).ToString(); }
-   }
-
-   public void ToggleOn() { if (Character.UpcastMethod != null && Quantity > 0) { QuantityDescriptionText.text = ((int)Quantity).ToString(); ContentPanel.SetActive(true); }}
-   private void ToggleOff() { ContentPanel.SetActive(false); }
 }
