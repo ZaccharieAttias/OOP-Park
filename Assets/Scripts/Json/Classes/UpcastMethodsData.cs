aae4d64cf3942257d6fe05ffc6dd5df58956c8f8
Nitzan <nitzantomer1998@gmail.com>
2024-02-16 09:53:06 +0200

    Improving Null Check

--- Assets/Scripts/Json/Classes/UpcastMethodsData.cs
+++ Assets/Scripts/Json/Classes/UpcastMethodsData.cs
@@ -5,20 +5,20 @@ public static class UpcastMethodsData
         var upcastMethod = character.UpcastMethod?.CharacterMethod;
         UpcastMethodData upcastMethodData = new()
         {
-            Owner = upcastMethod == null ? null : MethodsData.FindMethodOwner(character, upcastMethod),
+            Owner = upcastMethod is null ? null : MethodsData.FindMethodOwner(character, upcastMethod),
             Name = upcastMethod?.Name,
-            Amount = upcastMethod == null ? 0 : character.UpcastMethod.Amount
+            Amount = upcastMethod is null ? 0 : character.UpcastMethod.Amount
         };
 
         return upcastMethodData;
     }
     public static CharacterUpcastMethod UnpackData(CharacterData characterData)
     {
-        var upcastMethodData = characterData.UpcastMethod.Name == null ? null : CharactersData.CharactersManager.CharactersCollection.Find(character => character.Name == characterData.UpcastMethod.Owner).Methods.Find(method => method.Name == characterData.UpcastMethod.Name);
+        var upcastMethodData = characterData.UpcastMethod.Name is null ? null : CharactersData.CharactersManager.CharactersCollection.Find(character => character.Name == characterData.UpcastMethod.Owner).Methods.Find(method => method.Name == characterData.UpcastMethod.Name);
         
         CharacterUpcastMethod upcastMethod = new(upcastMethodData, characterData.UpcastMethod.Amount);
         
-        return upcastMethodData == null ? null : upcastMethod;
+        return upcastMethodData is null ? null : upcastMethod;
     }
 }
 
