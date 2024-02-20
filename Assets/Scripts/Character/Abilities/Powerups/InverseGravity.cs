abd40d83aacda550b44204cb58630dbf0c880220
Zaccharie Attias <zaccharieattias@gmail.com>
2024-02-20 19:08:24 +0200

    Create InverseGravity.cs

---
+++ Assets/Scripts/Character/Abilities/Powerups/InverseGravity.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "PowerUpEffects/InverseGravity")]
+public class InverseGravity : PowerupEffect
+{   
+    public float JumpForceIncrease = -40f;
+
+
+    public override void ActivatePower(GameObject player, float value)
+    {
+        JumpForceIncrease = value;
+        player.GetComponent<PlayerMovement>().JumpForce += JumpForceIncrease;
+        player.GetComponent<Rigidbody2D>().gravityScale = -player.GetComponent<Rigidbody2D>().gravityScale;
+        player.transform.localScale = new Vector3(player.transform.localScale.x, -player.transform.localScale.y, player.transform.localScale.z);
+    }
+
+    public override void DeactivatePower(GameObject player) 
+    { 
+        player.GetComponent<PlayerMovement>().JumpForce -= JumpForceIncrease;
+        player.GetComponent<Rigidbody2D>().gravityScale = -player.GetComponent<Rigidbody2D>().gravityScale; 
+        player.transform.localScale = new Vector3(player.transform.localScale.x, -player.transform.localScale.y, player.transform.localScale.z);
+    }
+}
