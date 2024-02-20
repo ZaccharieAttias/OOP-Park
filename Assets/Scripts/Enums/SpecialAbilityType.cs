684fe3291b0095d43cd1da258f82274d99e9227c
Zaccharie Attias <zaccharieattias@gmail.com>
2024-02-20 19:08:20 +0200

    Update SpecialAbilityType.cs

--- Assets/Scripts/Enums/SpecialAbilityType.cs
+++ Assets/Scripts/Enums/SpecialAbilityType.cs
@@ -1,1 +1,1 @@
-public enum SpecialAbilityType { General, Automatic, Manual, Jump, Gravity, Speed, FireballShoot, DoubleJump, WeakGravity, FastSpeed, GrapplingGun}
+public enum SpecialAbilityType { General, Automatic, Manual, Jump, Gravity, Speed, FireballShoot, DoubleJump, WeakGravity, FastSpeed, GrapplingGun, InverseGravity}
