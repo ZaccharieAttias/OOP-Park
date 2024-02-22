81878337d492d07a54cca455b45ce64a967a92e1
Nitzan <nitzantomer1998@gmail.com>
2024-02-22 11:02:51 +0200

    Improving Structure

--- Assets/Scripts/Json/Classes/UpcastMethodsData.cs
+++ Assets/Scripts/Json/Classes/UpcastMethodsData.cs
@@ -14,11 +14,13 @@ public static class UpcastMethodsData
     }
     public static UpcastMethod UnpackData(CharacterData characterData)
     {
-        var upcastMethodData = characterData.UpcastMethod.Name is null ? null : CharactersData.CharactersManager.CharactersCollection.Find(character => character.Name == characterData.UpcastMethod.Owner).Methods.Find(method => method.Name == characterData.UpcastMethod.Name);
-        if (upcastMethodData is null) return null;
+        var upcastMethodData = characterData.UpcastMethod.Name is null 
+            ? null 
+            : CharactersData.CharactersManager.CharactersCollection
+                .Find(character => character.Name == characterData.UpcastMethod.Owner).Methods
+                .Find(method => method.Name == characterData.UpcastMethod.Name);
 
-        UpcastMethod upcastMethod = new(upcastMethodData, characterData.UpcastMethod.Amount);
-        return upcastMethod;
+        return upcastMethodData is null ? null : new(upcastMethodData, characterData.UpcastMethod.Amount);
     }
 }
 
