c5951b897d87dcc8872a224a5c6a4c0d3b06be88
Nitzan <nitzantomer1998@gmail.com>
2024-02-22 13:11:23 +0200

    Implement Private Access Modifier Behavior

--- Assets/Scripts/UI/Buttons/AccessModifierButton.cs
+++ Assets/Scripts/UI/Buttons/AccessModifierButton.cs
@@ -8,7 +8,7 @@ public class AccessModifierButton : MonoBehaviour
     public int AccessModifierIndex;
     public int AccessModifierCount;
     public List<Color> AccessModifierColors;
-    
+
     public Attribute Attribute;
     public Method Method;
 
@@ -18,23 +18,38 @@ public class AccessModifierButton : MonoBehaviour
     public void Start() { InitializeProperties(); }
     private void InitializeProperties()
     {
-        AccessModifierIndex = Attribute != null ? Attribute.AccessModifier.GetHashCode() : Method.AccessModifier.GetHashCode();
-        AccessModifierCount = System.Enum.GetNames(typeof(AccessModifier)).Length;;
-        AccessModifierColors = new List<Color> { new Color32(0, 255, 0, 200), new Color32(255, 165, 0, 200), new Color32(255, 0, 0, 200) };
-        
+        AccessModifierIndex = Attribute is not null ? Attribute.AccessModifier.GetHashCode() : Method.AccessModifier.GetHashCode();
+        AccessModifierCount = System.Enum.GetNames(typeof(AccessModifier)).Length;
+        AccessModifierColors = new List<Color>
+        { 
+            new Color32(0, 255, 0, 200),
+            new Color32(255, 165, 0, 200),
+            new Color32(255, 0, 0, 200) 
+        };
+
         ButtonImage = GetComponent<Image>();
         ButtonImage.color = AccessModifierColors[AccessModifierIndex];
 
         GetComponent<Button>().onClick.AddListener(() => MarkAccessModifier());
     }
-    
+
+
     private void MarkAccessModifier()
-    { 
+    {
         AccessModifierIndex = (AccessModifierIndex + 1) % AccessModifierCount;
 
-        if (Attribute != null) Attribute.AccessModifier = (AccessModifier)AccessModifierIndex;
+        if (Attribute is not null) Attribute.AccessModifier = (AccessModifier)AccessModifierIndex;
         else Method.AccessModifier = (AccessModifier)AccessModifierIndex;
-        
+
         ButtonImage.color = AccessModifierColors[AccessModifierIndex];
+
+        if (AccessModifierIndex == 2 && RestrictionManager.Instance.AllowAccessModifiers)
+        {
+            Character currentCharacter = CharactersData.CharactersManager.CurrentCharacter;
+
+            if (Attribute is not null) foreach (Character child in currentCharacter.Childrens) AttributesData.AttributesManager.CancelAttributeReferences(child, Attribute);
+            else foreach (Character child in currentCharacter.Childrens) MethodsData.MethodsManager.CancelMethodReferences(child, Method);
+        }
+
     }
 }
