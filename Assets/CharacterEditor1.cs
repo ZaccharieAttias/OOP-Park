19bd6452d45271a9f7af9186afc7b94a32c61c65
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-06 15:59:50 +0300

    Refactoring/ Refreshing files

--- Assets/CharacterEditor1.cs
+++ Assets/CharacterEditor1.cs
@@ -1,22 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Assets.HeroEditor.Common.Scripts.CharacterScripts;
-using Assets.HeroEditor.Common.Scripts.Collections;
-using Assets.HeroEditor.Common.Scripts.Common;
-using Assets.HeroEditor.Common.Scripts.Data;
-using Assets.HeroEditor.Common.Scripts.ExampleScripts;
-using Assets.HeroEditor.InventorySystem.Scripts;
-using Assets.HeroEditor.InventorySystem.Scripts.Data;
-using Assets.HeroEditor.InventorySystem.Scripts.Elements;
-using Assets.HeroEditor4D.SimpleColorPicker.Scripts;
-using HeroEditor.Common;
-using HeroEditor.Common.Data;
-using HeroEditor.Common.Enums;
+﻿using HeroEditor.Common;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using System.IO;
 
 /// <summary>
 /// Character editor UI and behaviour.
@@ -25,11 +9,10 @@ public class CharacterEditor1 : MonoBehaviour
 {
     public CharacterBase Character;
 
-
     public void LoadFromJson()
     {
-        string path = $"C:\\Users\\nitza\\Desktop\\Applications\\Projects\\School\\OOP-Park\\Assets\\Resources\\CharactersData\\json\\test\\{CharactersData.CharactersManager.CurrentCharacter.Name}.json";
-        var json = System.IO.File.ReadAllText(path);
+        string path = Path.Combine(Application.dataPath, "Resources/CharactersData", UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, $"{CharactersData.CharactersManager.CurrentCharacter.Name}.json");
+        var json = File.ReadAllText(path);
 
         Character.FromJson(json);
     }
