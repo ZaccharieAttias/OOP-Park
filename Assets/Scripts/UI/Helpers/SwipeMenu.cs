2ed3c59afd7c3a185b4f2dd8b470c9383500a298
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-09 22:53:44 +0300

    fix the ui impression of the set

--- Assets/Scripts/UI/Helpers/SwipeMenu.cs
+++ Assets/Scripts/UI/Helpers/SwipeMenu.cs
@@ -5,15 +5,17 @@ using UnityEngine.UI;
 public class SwipeMenu : MonoBehaviour
 {
     public GameObject ScrollBar;
-    public float Scroll_pos = 0;
+    public float Scroll_pos;
     public float[] pos;
     public EncapsulationManager EncapsulationManager;
-    public float Scrollposition = 0;
+    public float Scrollposition;
 
     public void Start()
     {
         EncapsulationManager = GameObject.Find("Canvas/Popups").GetComponent<EncapsulationManager>();
         ScrollBar = EncapsulationManager.Popup.transform.Find("Background/Foreground/Set/ScrollView/ScrollbarVertical").gameObject;
+        Scroll_pos = 1;
+        Scrollposition = 1;
     }
     public void Update()
     {
@@ -21,28 +23,32 @@ public class SwipeMenu : MonoBehaviour
 
         float distance = 1f / (pos.Length - 1f);
         for (int i = 0; i < pos.Length; i++){
-            pos[i] = distance * i;
+            pos[i] = 1- distance * i;
         }
-        Array.Reverse(pos);
 
         if (Input.GetMouseButton (0))
             Scroll_pos = ScrollBar.GetComponent<Scrollbar>().value;
         else{
-            for (int i =0; i < pos.Length; i++){
-                if (Scroll_pos < pos[i] + (distance / 2) && Scroll_pos > pos[i] - (distance / 2)){
-                    ScrollBar.GetComponent<Scrollbar>().value = Mathf.Lerp(ScrollBar.GetComponent<Scrollbar>().value, pos[i], 0.1f);
+            for (int i = 0; i < pos.Length; i++){
+                float invertedScrollPos = 1 - Scroll_pos;
+                if (invertedScrollPos < pos[i] + (distance / 2) && invertedScrollPos > pos[i] - (distance / 2))
+                {
+                    ScrollBar.GetComponent<Scrollbar>().value = Mathf.Lerp(ScrollBar.GetComponent<Scrollbar>().value, 1-pos[i], 0.1f);
                 }
             }
         }
 
         int z=0;
+        int childCount = transform.childCount;
         for (int i = 0; i < pos.Length; i++){
-            if (Scroll_pos < pos[i] + (distance / 2) && Scroll_pos > pos[i] - (distance / 2)){
-                transform.GetChild(i).localScale = Vector2.Lerp(transform.GetChild(i).localScale, new Vector2(1f, 1f), 0.1f);
+            float invertedScrollPos = 1 - Scroll_pos;
+            if (invertedScrollPos < pos[i] + (distance / 2) && invertedScrollPos > pos[i] - (distance / 2))
+            {
+                transform.GetChild(childCount - i - 1).localScale = Vector2.Lerp(transform.GetChild(childCount - i - 1).localScale, new Vector2(1f, 1f), 0.1f);
                 z=i;
                 for (int j =0; j<pos.Length;j++){
                     if (j!= i)
-                        transform.GetChild(j).localScale = Vector2.Lerp (transform.GetChild(j).localScale, new Vector2(0.8f,0.8f), 0.1f);
+                        transform.GetChild(childCount - j - 1).localScale = Vector2.Lerp (transform.GetChild(childCount - j - 1).localScale, new Vector2(0.8f,0.8f), 0.1f);
                 }
             }
         }
