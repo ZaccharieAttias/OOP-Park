444d1028520940d13338bc67e149d268655c0ba9
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-10 21:19:29 +0300

    Update CharacterEditor1.cs

--- Assets/CharacterEditor1.cs
+++ Assets/CharacterEditor1.cs
@@ -11,12 +11,21 @@ public class CharacterEditor1 : MonoBehaviour
 
     public void Start()
     {
-        Character = GameObject.Find("Player").GetComponent<CharacterBase>();
+        if (GameObject.Find("Player") != null)
+        {
+            Character = GameObject.Find("Player").GetComponent<CharacterBase>();
+        }
     }
-
     public void LoadFromJson()
     {
         string path = Path.Combine(Application.dataPath, "Resources/CharactersData", UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, $"{CharactersData.CharactersManager.CurrentCharacter.Name}.json");
+        var json = File.ReadAllText(path);
+
+        Character.FromJson(json);
+    }
+    public void OnlineLoadFromJson()
+    {
+        string path = Path.Combine(Application.dataPath, "Resources/CharactersData", UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Character 1.json");
         var json = File.ReadAllText(path);
 
         Character.FromJson(json);
