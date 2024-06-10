5c8c4e15e2998df141198e449fa707d9667be816
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-10 18:45:08 +0300

    Update EncapsulationManager.cs

--- Assets/Scripts/UI/Popups/EncapsulationManager.cs
+++ Assets/Scripts/UI/Popups/EncapsulationManager.cs
@@ -52,6 +52,7 @@ public class EncapsulationManager : MonoBehaviour
 
     public void Update()
     {
+        if (RestrictionManager.Instance.AllowEncapsulation is false) return;
         List<Attribute> SetList = CharactersData.CharactersManager.CurrentCharacter.Attributes.Where(item => item.Setter).ToList();
         if (SetList.Count>0)
             if (Input.GetKeyDown(KeyCode.I) && RestrictionManager.Instance.AllowEncapsulation && GameObject.Find("Canvas/Menus/Gameplay").activeSelf)
