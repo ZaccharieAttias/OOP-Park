444197bbb38a6895c53d9179cd5d7068b7ab3e91
Nitzan <nitzantomer1998@gmail.com>
2024-06-16 16:34:24 +0300

    Improving Structure & Performances

--- Assets/Scripts/UI/Popups/AttributesManager.cs
+++ Assets/Scripts/UI/Popups/AttributesManager.cs
@@ -7,55 +7,54 @@ using UnityEngine.UI;
 
 public class AttributesManager : MonoBehaviour
 {
+    [Header("UI Elements")]
     public GameObject Popup;
-    public List<Attribute> AttributesCollection;
-    
     public GameObject AttributeButton;
     public Transform AttributesContentPanel;
 
-    
-    public void Start() { InitializeProperties(); }
-    private void InitializeProperties()
+    [Header("Attribute Data")]
+    public List<Attribute> AttributesCollection = new();
+    public List<GameObject> AttributeGameObjects = new();
+
+
+    public void Start()
+    {
+        InitializeProperties();
+    }
+    public void InitializeProperties()
     {
         Popup = GameObject.Find("Canvas/Popups/Attributes");
-        AttributesCollection = new List<Attribute>();
-
         AttributeButton = Resources.Load<GameObject>("Buttons/Default");
         AttributesContentPanel = Popup.transform.Find("Background/Foreground/Buttons/ScrollView/ViewPort/Content");
-        
-        Button popupToggleOn = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Attributes/Buttons/Edit").GetComponent<Button>();
+
+        AttributesCollection = new();
+        AttributeGameObjects = new();
+
+        var popupToggleOn = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Attributes/Buttons/Edit").GetComponent<Button>();
         popupToggleOn.onClick.AddListener(() => ToggleOn());
 
-        Button popupToggleOff = Popup.transform.Find("Background/Foreground/Buttons/Close").GetComponent<Button>();
+        var popupToggleOff = Popup.transform.Find("Background/Foreground/Buttons/Close").GetComponent<Button>();
         popupToggleOff.onClick.AddListener(() => ToggleOff());
     }
 
-
-    private void LoadPopup()
+    public void LoadPopup()
     {
-        ClearContentPanel();
-        
-        foreach (Attribute attribute in AttributesCollection)
+        if (AttributeGameObjects.Count == 0)
         {
-            GameObject attributeGameObject = Instantiate(AttributeButton, AttributesContentPanel);
-            attributeGameObject.name = attribute.Name;
+            BuildAttributeGameObjects();
+        }
 
-            TMP_Text attributeButtonText = attributeGameObject.GetComponentInChildren<TMP_Text>();
-            attributeButtonText.text = attribute.Name;
-
+        foreach (var attributeGameObject in AttributeGameObjects)
+        {
             Image image = attributeGameObject.GetComponent<Image>();
-            image.color = CharactersData.CharactersManager.CurrentCharacter.Attributes.Any(item => item.Name == attribute.Name) ? Color.green : Color.white;
-            
-            Button attributeButton = attributeGameObject.GetComponent<Button>();
-            attributeButton.onClick.AddListener(() => MarkAttribute(attributeGameObject, attribute));
+            image.color = CharactersData.CharactersManager.CurrentCharacter.Attributes.Any(item => item.Name == attributeGameObject.name) ? Color.green : Color.white;
         }
     }
-    private void ClearContentPanel() { foreach (Transform attributeTransform in AttributesContentPanel) Destroy(attributeTransform.gameObject); }
-    private void MarkAttribute(GameObject attributeGameObject, Attribute attribute)
+    public void MarkAttribute(GameObject attributeGameObject, Attribute attribute)
     {
-        CharacterB currentCharacter = CharactersData.CharactersManager.CurrentCharacter;
+        var currentCharacter = CharactersData.CharactersManager.CurrentCharacter;
         var currentAttribute = currentCharacter.Attributes.FirstOrDefault(item => item.Name == attribute.Name);
-    
+
         if (currentAttribute is null)
         {
             Attribute newAttribute = new(attribute, currentCharacter.Name);
@@ -65,32 +64,65 @@ public class AttributesManager : MonoBehaviour
         else
         {
             bool isAttributeOwner = currentCharacter.Name == currentAttribute.Owner;
-            if (isAttributeOwner) CancelAttributeReferences(currentCharacter, currentAttribute);
-            else currentCharacter.Attributes.Remove(currentAttribute);
+            if (isAttributeOwner)
+            {
+                CancelAttributeReferences(currentCharacter, currentAttribute);
+            }
+            
+            else
+            {
+                currentCharacter.Attributes.Remove(currentAttribute);
+            }
         }
 
         Image image = attributeGameObject.GetComponent<Image>();
         image.color = currentAttribute is null ? Color.green : Color.white;
     }
+    
     public void CancelAttributeReferences(CharacterB character, Attribute attribute)
     {
         var referencedAttribute = character.Attributes.FirstOrDefault(item => item == attribute);
-        if (referencedAttribute is not null) character.Attributes.Remove(referencedAttribute);
+        if (referencedAttribute is not null)
+        {
+            character.Attributes.Remove(referencedAttribute);
+        }
 
         var dependentMethod = character.Methods.FirstOrDefault(item => item.Attribute == attribute);
-        if (dependentMethod is not null) character.Methods.Remove(dependentMethod);
+        if (dependentMethod is not null)
+        {
+            character.Methods.Remove(dependentMethod);
+        }
 
-        foreach (CharacterB child in character.Childrens) CancelAttributeReferences(child, attribute);
+        foreach (var child in character.Childrens)
+        {
+            CancelAttributeReferences(child, attribute);
+        }
+    }
+    public void BuildAttributeGameObjects()
+    {
+        foreach (var attribute in AttributesCollection)
+        {
+            var attributeGameObject = Instantiate(AttributeButton, AttributesContentPanel);
+            attributeGameObject.name = attribute.Name;
+
+            var attributeButtonText = attributeGameObject.GetComponentInChildren<TMP_Text>();
+            attributeButtonText.text = attribute.Name;
+
+            var attributeButton = attributeGameObject.GetComponent<Button>();
+            attributeButton.onClick.AddListener(() => MarkAttribute(attributeGameObject, attribute));
+
+            AttributeGameObjects.Add(attributeGameObject);
+        }
     }
 
     public void ToggleOn()
     {
         LoadPopup();
-        Popup.SetActive(true);    
+        Popup.SetActive(true);
     }
     public void ToggleOff()
     {
         CharactersData.CharactersManager.DisplayCharacter(CharactersData.CharactersManager.CurrentCharacter);
-        Popup.SetActive(false); 
+        Popup.SetActive(false);
     }
 }
