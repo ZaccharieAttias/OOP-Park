5c4561ad5be484fdf00d625c877a6a725fc98fe2
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-24 17:15:12 +0300

    Update SpecialAbilitiesData.cs

--- Assets/Scripts/Json/Classes/SpecialAbilitiesData.cs
+++ Assets/Scripts/Json/Classes/SpecialAbilitiesData.cs
@@ -4,10 +4,14 @@ using System.IO;
 using UnityEngine;
 
 
+
+
 public static class SpecialAbilitiesData
 {
     public static string FilePath;
     public static SpecialAbilityManager SpecialAbilityManager;
+
+
 
 
     public static void Initialize(string folderPath)
@@ -17,30 +21,73 @@ public static class SpecialAbilitiesData
     }
 
 
-    // public static void Save() { File.WriteAllText(FilePath, Serialize(SpecialAbilitiesManager.SpecialAbilitiesCollection)); }
-    // public static void Load() { SpecialAbilitiesManager.SpecialAbilitiesCollection = Deserialize(File.ReadAllText(FilePath)); }
-    // public static void Load(string filename) { SpecialAbilitiesManager.SpecialAbilitiesCollection = Deserialize(File.ReadAllText(filename)); }
 
-    // public static string Serialize(Dictionary<SpecialAbilityType, List<SpecialAbility>> specialAbilities) { return JsonConvert.SerializeObject(specialAbilities, Formatting.Indented); }
-    // public static Dictionary<SpecialAbilityType, List<SpecialAbility>> Deserialize(string json) { return JsonConvert.DeserializeObject<Dictionary<SpecialAbilityType, List<SpecialAbility>>>(json); }
 
-    // public static SpecialAbilityData PackData(CharacterB character)
-    // {
-    //     SpecialAbilityData specialAbilityData = new()
-    //     {
-    //         Name = character.SpecialAbility.Name,
-    //         Type = character.SpecialAbility.Type
-    //     };
+    public static void Save() { File.WriteAllText(FilePath, Serialize(SpecialAbilityManager.SpecialAbilitiesDictionary)); }
+    public static void Load() { SpecialAbilityManager.SpecialAbilitiesCollection = Deserialize(File.ReadAllText(FilePath)); }
+    public static void Load(string filename) { SpecialAbilityManager.SpecialAbilitiesCollection = Deserialize(File.ReadAllText(filename)); }
 
-    //     return specialAbilityData;
-    // }
-    // public static SpecialAbility UnpackData(CharacterData characterData)
-    // {
-    //     string specialAbilityName = characterData.SpecialAbility.Name;
-    //     SpecialAbilityType specialAbilityType = characterData.SpecialAbility.Type;
 
-    //     return SpecialAbilitiesManager.SpecialAbilitiesCollection[specialAbilityType].Find(ability => ability.Name == specialAbilityName);
-    // }
+    public static string Serialize(Dictionary<SpecialAbilityType, List<SpecialAbility>> specialAbilities) { return JsonConvert.SerializeObject(specialAbilities, Formatting.Indented); }
+    public static List<SpecialAbilityObject> Deserialize(string json) { return temp(json); }
+
+
+    public static SpecialAbilityData PackData(CharacterB character)
+    {
+        if (character.SpecialAbility == null) return null;
+        
+        SpecialAbilityData specialAbilityData = new()
+        {
+            Name = character.SpecialAbility.Name,
+            Type = character.SpecialAbility.Type,
+        };
+
+
+        return specialAbilityData;
+    }
+    public static SpecialAbility UnpackData(CharacterData characterData)
+    {
+        string specialAbilityName = characterData.SpecialAbility?.Name;
+        if (specialAbilityName == null) return null;
+        SpecialAbilityType specialAbilityType = characterData.SpecialAbility.Type;
+
+
+        return SpecialAbilityManager.SpecialAbilitiesCollection.Find(ability => ability.SpecialAbility.Name == specialAbilityName).SpecialAbility;
+    }
+
+
+
+
+    public static List<SpecialAbilityObject> temp(string json)
+    {
+        Dictionary<SpecialAbilityType, List<SpecialAbility>> specialAbilities = JsonConvert.DeserializeObject<Dictionary<SpecialAbilityType, List<SpecialAbility>>>(json);
+
+
+        List<SpecialAbilityObject> specialAbilityObjects = new();
+        foreach (KeyValuePair<SpecialAbilityType, List<SpecialAbility>> specialAbilityType in specialAbilities)
+        {
+            foreach (SpecialAbility specialAbility in specialAbilityType.Value)
+            {
+                SpecialAbilityObject specialAbilityObject = new(specialAbility);
+
+
+                if (specialAbility.Name.Replace(" ", "") == specialAbilityType.Key.ToString().Replace(" ", ""))
+                {
+                    if (specialAbility.Name == "General") specialAbilityObject.Parent = null;
+                    else continue;
+                }
+                else
+                {
+                    specialAbilityObject.Parent = specialAbilityObjects.Find(ability => ability.SpecialAbility.Name.Replace(" ", "") == specialAbilityType.Key.ToString().Replace(" ", ""));
+                    specialAbilityObject.Parent.Childrens.Add(specialAbilityObject);
+                }
+                specialAbilityObject.Name = specialAbility.Name;
+                specialAbilityObjects.Add(specialAbilityObject);
+            }
+        }
+        return specialAbilityObjects;
+    }
+    public static void SetPath(string path) { FilePath = path; }
 }
 
 
