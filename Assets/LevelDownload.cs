a666ca7355cbc17832a56157fd614f9ca8ba3629
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-24 17:10:07 +0300

    Update LevelDownload.cs

--- Assets/LevelDownload.cs
+++ Assets/LevelDownload.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 using UnityEngine.Networking;
 using LootLocker.Requests;
 using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
 
 public class LevelDownload : MonoBehaviour
 {
@@ -17,10 +19,7 @@ public class LevelDownload : MonoBehaviour
 
     public void DownloadLevelData()
     {
-        //filtrez les assets pour obtenir uniquement les Tilemaps
-        
-
-        LootLockerSDKManager.GetAssetListWithCount(10, (response) =>
+        LootLockerSDKManager.GetAssetListWithCount(50, (response) =>
         {
             foreach (Transform child in LevelDataEntryContent)
             {
@@ -45,13 +44,55 @@ public class LevelDownload : MonoBehaviour
                 j = FindIndex(levelFiles,"FILE");
                 if (j == -1)
                 {
-                    Debug.Log("No primary thumbnail found");
+                    Debug.Log("No file type found");
                     return;
                 }
-                displayItem.GetComponent<LevelInitializer>().TextFileURL = levelFiles[j].url.ToString();
+                displayItem.GetComponent<LevelInitializer>().DataFileURL = levelFiles[j].url.ToString();
             }
 
         }, null, true, new Dictionary<string, string>() { { "Context", "TileMap" } });
+    }
+    public void DownloadLevelTreeData(string levelName)
+    {
+        LootLockerSDKManager.GetAssetListWithCount(50, (response) =>
+        {
+            for (int i = 0; i < response.assets.Length; i++)
+            {
+                if (response.assets[i].name == levelName)
+                {
+                    LootLockerFile[] levelFiles = response.assets[i].files;
+                    for (int j = 0; j < levelFiles.Length; j++)
+                    {
+                        StartCoroutine(DownloadTextFile(levelFiles[j].url.ToString(), levelName, j));
+                    }
+                }
+            }
+        }, null, true, new Dictionary<string, string>() { { "Context", "CharacterTree" } });
+    }
+    private IEnumerator DownloadTextFile(string url, string LevelName, int index)
+    {
+        UnityWebRequest www = UnityWebRequest.Get(url);
+        yield return www.SendWebRequest();
+
+        string type = "";
+        switch (index)
+        {
+            case 0:
+                type = "Attributes";
+                break;
+            case 1:
+                type = "Characters";
+                break;
+            case 2:
+                type = "Methods";
+                break;
+            case 3:
+                type = "SpecialAbilities";
+                break;
+        }
+
+        string filePath = Directory.GetCurrentDirectory() + "/Assets/Resources/Screenshots/" + LevelName + "/" + type + ".json";
+        File.WriteAllText(filePath, www.downloadHandler.text);
     }
     public int FindIndex(LootLockerFile[] files, string purpose)
     {
