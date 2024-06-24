5eb8c93b805a2392218823e027e56ef8797c97d0
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-24 17:15:54 +0300

    Update SwapScreen.cs

--- Assets/SwapScreen.cs
+++ Assets/SwapScreen.cs
@@ -5,15 +5,30 @@ using UnityEngine.UI;
 
 public class SwapScreen : MonoBehaviour
 {
-    public Button SwapButton;
+    public Button SwapButtonToGameplay;
+    public Button SwapButtonToCharacterCenter;
     public CharacterEditor1 CharacterEditor;
+    public bool firstTime = true;
 
     public void Start()
     {
         CharacterEditor = GameObject.Find("Scripts/CharacterEditor").GetComponent<CharacterEditor1>();
-        SwapButton = GameObject.Find("Canvas/Menus/CharacterCenter/SwapScreen").GetComponent<Button>();
-        SwapButton.onClick.AddListener(() => CharacterEditor.LoadFromJson());
+        SwapButtonToGameplay = GameObject.Find("Canvas/Menus/CharacterCenter/SwapScreen").GetComponent<Button>();
+        SwapButtonToGameplay.onClick.AddListener(() => CharacterEditor.LoadFromJson());
 
-        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "C2L4") SwapButton.gameObject.SetActive(false);
+        SwapButtonToCharacterCenter = GameObject.Find("Canvas/Menus/Gameplay/SwapScreen").GetComponent<Button>();
+
+        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "C2L4") SwapButtonToGameplay.gameObject.SetActive(false);
+    }
+    public void FisrtSwap()
+    {
+        if (firstTime)
+        {
+            firstTime = false;
+            SwapButtonToCharacterCenter.onClick.RemoveAllListeners();
+            if (RestrictionManager.Instance.AllowSingleInheritance)
+                SwapButtonToCharacterCenter.onClick.AddListener(() => GameObject.Find("Canvas/Popups").GetComponent<CharactersCreationManager>().ToggleOn());
+            SwapButtonToCharacterCenter.onClick.AddListener(() => GameObject.Find("Canvas/Menus").GetComponent<GameplayManager>().ToggleOff());
+        }
     }
 }
