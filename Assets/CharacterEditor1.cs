be7cc20ffbbc0cbc441f60f5920f22c01bae7356
Zaccharie Attias <zaccharieattias@gmail.com>
2024-06-24 17:09:36 +0300

    Update CharacterEditor1.cs

--- Assets/CharacterEditor1.cs
+++ Assets/CharacterEditor1.cs
@@ -1,13 +1,37 @@
 ﻿using HeroEditor.Common;
 using UnityEngine;
 using System.IO;
+using Assets.HeroEditor.Common.Scripts.Collections;
+using Assets.HeroEditor.InventorySystem.Scripts.Elements;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using Assets.HeroEditor4D.SimpleColorPicker.Scripts;
+using static Assets.HeroEditor.Common.Scripts.EditorScripts.CharacterEditor;
 
-/// <summary>
-/// Character editor UI and behaviour.
-/// </summary>
 public class CharacterEditor1 : MonoBehaviour
 {
     public CharacterBase Character;
+    public string AssetStoreLink;
+
+
+    [Header("Public")]
+    public SpriteCollection SpriteCollection;
+    public IconCollection IconCollection;
+    public Transform Tabs;
+    public ScrollInventory Inventory;
+    public Text ItemName;
+
+
+    [Header("Other")]
+    public List<Toggle> EditionToggles;
+    public string EditionFilter;
+    public List<string> PaintParts;
+    public Button PaintButton;
+    public ColorPicker ColorPicker;
+    public List<string> CollectionSorting;
+    public List<CollectionBackground> CollectionBackgrounds;
+    public string FilePickerPath;
+
 
     public void Start()
     {
@@ -15,10 +39,11 @@ public class CharacterEditor1 : MonoBehaviour
         {
             Character = GameObject.Find("Player").GetComponent<CharacterBase>();
         }
+        AssetStoreLink = "http://u3d.as/QCQ";
     }
     public void LoadFromJson()
     {
-        string path = Path.Combine(Application.dataPath, "Resources/CharactersData", UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, $"{CharactersData.CharactersManager.CurrentCharacter.Name}.json");
+        string path = Path.Combine(Application.dataPath, $"Resources/CharactersData/All/{CharactersData.CharactersManager.CurrentCharacter.Name}.json");
         var json = File.ReadAllText(path);
 
         Character.FromJson(json);
@@ -32,7 +57,14 @@ public class CharacterEditor1 : MonoBehaviour
     }
     public void OnlineLoadFromJson()
     {
-        string path = Path.Combine(Application.dataPath, "Resources/CharactersData", UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Character 1.json");
+        string path = Path.Combine(Application.dataPath, "Resources/CharactersData", UnityEngine.SceneManagement.SceneManager.GetActiveScene().name, "Character 0.json");
+        var json = File.ReadAllText(path);
+
+        Character.FromJson(json);
+    }
+    public void SpecialLoadFromJson()
+    {
+        string path = Path.Combine(Application.dataPath, "Resources/CharactersData", $"{UnityEngine.SceneManagement.SceneManager.GetActiveScene().name}",$"{CharactersData.CharactersManager.CurrentCharacter.Name}.json");
         var json = File.ReadAllText(path);
 
         Character.FromJson(json);
