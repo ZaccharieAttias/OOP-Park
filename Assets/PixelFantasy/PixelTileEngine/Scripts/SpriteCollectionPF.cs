955f90351f587251d12f0173855cff58587585ae
Zaccharie Attias <zaccharieattias@gmail.com>
2024-07-05 11:32:21 +0300

    Update SpriteCollectionPF.cs

--- Assets/PixelFantasy/PixelTileEngine/Scripts/SpriteCollectionPF.cs
+++ Assets/PixelFantasy/PixelTileEngine/Scripts/SpriteCollectionPF.cs
@@ -17,7 +17,7 @@ namespace Assets.PixelFantasy.PixelTileEngine.Scripts
         public List<Tileset> CoverTilesets;
         public List<Sprite> PropsSprites;
         public List<Sprite> OtherSprites;
-        public List<Sprite> PlayerSprite;
+        public List<Sprite> GamePlaySprite;
         public Sprite DeleteSprite;
 
         #if UNITY_EDITOR
@@ -28,7 +28,7 @@ namespace Assets.PixelFantasy.PixelTileEngine.Scripts
             CoverTilesets = LoadTextures(TilesFolder, "Cover").Select(i => new Tileset(i)).ToList();
             PropsSprites = LoadSprites(TilesFolder, "Props");
             OtherSprites = LoadSprites(TilesFolder, "Ladder").Union(LoadSprites(TilesFolder, "Bridge")).ToList();
-            PlayerSprite = LoadSprites(TilesFolder, "Player");
+            GamePlaySprite = LoadSprites(TilesFolder, "GamePlay");
             UnityEditor.EditorUtility.SetDirty(this);
             Debug.Log("Refresh done!");
         }
