cff9b39e5f39b931b204815574ebb6cad903ebba
Zaccharie Attias <94083271+ZaccharieAttias@users.noreply.github.com>
2024-07-14 17:19:38 +0300

    Update SeedCollections.cs

--- Assets/Scripts/Helpers/SeedCollections.cs
+++ Assets/Scripts/Helpers/SeedCollections.cs
@@ -31,7 +31,7 @@ public class SeedCollections : MonoBehaviour
         InitializeAttributesCollection();
         InitializeMethodsCollection();
         InitializeSpecialAbilityCollection();
-        InitializeCharactersCollection();
+        if (!RestrictionManager.Instance.OnlineGame) InitializeCharactersCollection();
 
         SceneManagement.GameplayInfo = BuildGameplayInfo();
     }
