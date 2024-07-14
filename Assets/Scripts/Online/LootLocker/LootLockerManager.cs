1197bfa02546a880253f02c2879452e0716cd871
Zaccharie Attias <94083271+ZaccharieAttias@users.noreply.github.com>
2024-07-14 17:19:40 +0300

    Update LootLockerManager.cs

--- Assets/Scripts/Online/LootLocker/LootLockerManager.cs
+++ Assets/Scripts/Online/LootLocker/LootLockerManager.cs
@@ -6,12 +6,20 @@ using LootLocker.Requests;
 
 public class LootLockerManager : MonoBehaviour
 {
+    public GameObject buttons;
     public void Start()
     {
         LootLockerSDKManager.StartGuestSession((response) =>
         {
             if (response.success)
             {   
+                buttons.SetActive(true);
+                
+                // si GameObject.Find("LevelManager") a le composant LevelDownload alors on appelle la méthode DownloadLevelData
+                if (GameObject.Find("LevelManager").GetComponent<LevelDownload>())
+                {
+                    GameObject.Find("LevelManager").GetComponent<LevelDownload>().DownloadLevelData();
+                }
             }
             else
             {
