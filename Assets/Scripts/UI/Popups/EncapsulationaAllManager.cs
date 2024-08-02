3728182145d8538110dff77c1f5c59efe0a629d2
Nitzan <nitzantomer1998@gmail.com>
2024-08-02 15:23:43 +0300

    Updating EncapsulationAllManager Structure

--- Assets/Scripts/UI/Popups/EncapsulationaAllManager.cs
+++ Assets/Scripts/UI/Popups/EncapsulationaAllManager.cs
@@ -7,59 +7,76 @@ using UnityEngine.UI;
 
 public class EncapsulationaAllManager : MonoBehaviour
 {
+    [Header("UI Elements")]
     public GameObject Popup;
-
     public Transform SetContent;
     public Transform GetContent;
 
-    public List<Attribute> SetCollection;
-    public List<Attribute> GetCollection;
-
+    [Header("Buttons")]
     public GameObject ButtonPrefab;
     public GameObject allButton;
     public Button CloseButton;
 
+    [Header("Attribute Collections")]
+    public List<Attribute> SetCollection;
+    public List<Attribute> GetCollection;
 
-    public void Start() { InitializeProperties(); }
-    private void InitializeProperties()
+
+    public void Start()
+    {
+        InitializeUIElements();
+        InitializeButtons();
+        InitializeAttributeCollections();
+    }
+    public void InitializeUIElements()
     {
         Popup = GameObject.Find("Canvas/Popups/EncapsulationAll");
-
-        SetCollection = new List<Attribute>();
-        GetCollection = new List<Attribute>();
-
         SetContent = Popup.transform.Find("Background/Foreground/Set/Back/ScrollView/Viewport/Content").GetComponent<Transform>();
         GetContent = Popup.transform.Find("Background/Foreground/Get/Back/ScrollView/Viewport/Content").GetComponent<Transform>();
-
+    }
+    public void InitializeButtons()
+    {
         ButtonPrefab = Resources.Load<GameObject>("Buttons/Default");
         allButton = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Description/All");
-        CloseButton = Popup.transform.Find("Background/Foreground/Buttons/Close").GetComponent<Button>();
+        allButton.GetComponent<Button>().onClick.AddListener(ToggleOn);
+        allButton.SetActive(RestrictionManager.Instance.AllowEncapsulation);
 
-        if (RestrictionManager.Instance.AllowEncapsulation)
-        {
-            allButton.GetComponent<Button>().onClick.AddListener(() => ToggleOn());
-            allButton.SetActive(true);
-            CloseButton.onClick.AddListener(() => ToggleOff());
-        }
+        CloseButton = Popup.transform.Find("Background/Foreground/Buttons/Close").GetComponent<Button>();
+        CloseButton.onClick.AddListener(ToggleOff);
+
+    }
+    public void InitializeAttributeCollections()
+    {
+        SetCollection = new List<Attribute>();
+        GetCollection = new List<Attribute>();
     }
 
-    private void LoadPopup()
+    public void LoadPopup()
     {
         ClearContentPanel();
-
-        var currentCharacter = CharactersData.CharactersManager.CurrentCharacter;
-        SetCollection = currentCharacter.Attributes.Where(item => item.Setter is true).ToList();
-        GetCollection = currentCharacter.Attributes.Where(item => item.Getter is true).ToList();
-
-        Load("Set");
-        Load("Get");
+        PopulateCollections();
+        LoadAttributes("Set");
+        LoadAttributes("Get");
     }
-    private void ClearContentPanel()
+    public void ClearContentPanel()
     {
-        SetContent.Cast<Transform>().ToList().ForEach(attributeTransform => Destroy(attributeTransform.gameObject));
-        GetContent.Cast<Transform>().ToList().ForEach(attributeTransform => Destroy(attributeTransform.gameObject));
+        foreach (Transform attributeTransform in SetContent)
+        {
+            Destroy(attributeTransform.gameObject);
+        }
+        
+        foreach (Transform attributeTransform in GetContent)
+        {
+            Destroy(attributeTransform.gameObject);
+        }
     }
-    public void Load(string type)
+    public void PopulateCollections()
+    {
+        var currentCharacter = CharactersData.CharactersManager.CurrentCharacter;
+        SetCollection = currentCharacter.Attributes.Where(attribute => attribute.Setter).ToList();
+        GetCollection = currentCharacter.Attributes.Where(attribute => attribute.Getter).ToList();
+    }
+    public void LoadAttributes(string type)
     {
         var collection = (type == "Get") ? GetCollection : SetCollection;
         var content = (type == "Get") ? GetContent : SetContent;
@@ -69,20 +86,22 @@ public class EncapsulationaAllManager : MonoBehaviour
             GameObject attributeGameObject = Instantiate(ButtonPrefab, content);
             attributeGameObject.name = $"{attribute.Name} {type}";
 
-            TMP_Text ButtonText = attributeGameObject.GetComponentInChildren<TMP_Text>();
-            ButtonText.text = attribute.Name;
+            TMP_Text buttonText = attributeGameObject.GetComponentInChildren<TMP_Text>();
+            buttonText.text = attribute.Name;
         }
     }
 
     public void ToggleOn()
     {
         SceneManagement.ScenePause("EncapsulationAllManager");
+
         LoadPopup();
         Popup.SetActive(true);
     }
     public void ToggleOff()
     {
         SceneManagement.SceneResume("EncapsulationAllManager");
+
         Popup.SetActive(false);
     }
 }
