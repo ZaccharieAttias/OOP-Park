c3a0764c6ce82997eae80bc117363468e960a823
Nitzan <nitzantomer1998@gmail.com>
2024-08-04 13:51:04 +0300

    Updating Access Modifier Button Structure

--- Assets/Scripts/UI/Buttons/AccessModifierButton.cs
+++ Assets/Scripts/UI/Buttons/AccessModifierButton.cs
@@ -5,42 +5,50 @@ using UnityEngine.UI;
 
 public class AccessModifierButton : MonoBehaviour
 {
+    [Header("Attributes")]
     public int AccessModifierIndex;
     public int AccessModifierCount;
     public List<Color> AccessModifierColors;
 
+    [Header("Associated Data")]
     public Attribute Attribute;
     public Method Method;
 
+    [Header("UI Elements")]
     public Image ButtonImage;
 
 
-    public void Start() { InitializeProperties(); }
-    private void InitializeProperties()
+    public void Start()
     {
-        AccessModifierIndex = Attribute is not null ? Attribute.AccessModifier.GetHashCode() : Method.AccessModifier.GetHashCode();
+        InitializeAttributes();
+        InitializeUIElements();
+    }
+    public void InitializeAttributes()
+    {
+        AccessModifierIndex = Attribute != null ? Attribute.AccessModifier.GetHashCode() : Method.AccessModifier.GetHashCode();
         AccessModifierCount = System.Enum.GetNames(typeof(AccessModifier)).Length;
         AccessModifierColors = new List<Color>
-        { 
+        {
             new Color32(0, 255, 0, 200),
             new Color32(255, 165, 0, 200),
-            new Color32(255, 0, 0, 200) 
+            new Color32(255, 0, 0, 200)
         };
-
+    }
+    public void InitializeUIElements()
+    {
         ButtonImage = GetComponent<Image>();
         ButtonImage.color = AccessModifierColors[AccessModifierIndex];
 
         GetComponent<Button>().onClick.AddListener(() => MarkAccessModifier());
     }
 
-
-    private void MarkAccessModifier()
+    public void MarkAccessModifier()
     {
         if (CharactersData.CharactersManager.CurrentCharacter.IsOriginal) return;
 
         AccessModifierIndex = (AccessModifierIndex + 1) % AccessModifierCount;
 
-        if (Attribute is not null) Attribute.AccessModifier = (AccessModifier)AccessModifierIndex;
+        if (Attribute != null) Attribute.AccessModifier = (AccessModifier)AccessModifierIndex;
         else Method.AccessModifier = (AccessModifier)AccessModifierIndex;
 
         ButtonImage.color = AccessModifierColors[AccessModifierIndex];
@@ -49,9 +57,21 @@ public class AccessModifierButton : MonoBehaviour
         {
             CharacterB currentCharacter = CharactersData.CharactersManager.CurrentCharacter;
 
-            if (Attribute is not null) foreach (CharacterB child in currentCharacter.Childrens) AttributesData.AttributesManager.CancelAttributeReferences(child, Attribute);
-            else foreach (CharacterB child in currentCharacter.Childrens) MethodsData.MethodsManager.CancelMethodReferences(child, Method);
-        }
+            if (Attribute != null)
+            {
+                foreach (CharacterB child in currentCharacter.Childrens)
+                {
+                    AttributesData.AttributesManager.CancelAttributeReferences(child, Attribute);
+                }
+            }
 
+            else
+            {
+                foreach (CharacterB child in currentCharacter.Childrens)
+                {
+                    MethodsData.MethodsManager.CancelMethodReferences(child, Method);
+                }
+            }
+        }
     }
 }
