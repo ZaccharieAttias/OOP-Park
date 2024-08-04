83d7ba43adab49563882d10748473580d5faafe3
Nitzan <nitzantomer1998@gmail.com>
2024-08-04 13:48:06 +0300

    Updating Characters Manager Structure

--- Assets/Scripts/UI/Menus/CharactersManager.cs
+++ Assets/Scripts/UI/Menus/CharactersManager.cs
@@ -8,51 +8,62 @@ using UnityEngine.SceneManagement;
 
 public class CharactersManager : MonoBehaviour
 {
+    [Header("Scripts")]
+    public CharactersTreeManager CharactersTreeManager;
+    public Powerup Powerup;
+
+    [Header("UI Elements")]
     public GameObject Menu;
-
-    public List<CharacterB> CharactersCollection;
-    public CharacterB CurrentCharacter;
-
     public TMP_Text NameText;
     public TMP_Text DescriptionText;
-
-    public GameObject DefaultButton;
-    public GameObject DeleteButton;
-    public GameObject GameplayButton;
-
     public Transform AttributesContentPanel;
     public Transform MethodsContentPanel;
     public Transform SpecialAbilityContentPanel;
 
-    public CharactersTreeManager CharactersTreeManager;
-
+    [Header("Buttons")]
+    public GameObject DefaultButton;
+    public GameObject DeleteButton;
+    public GameObject GameplayButton;
     public GameObject GetButton;
     public GameObject SetButton;
-
     public GameObject AttributePlusButton;
     public GameObject MethodPlusButton;
 
-    public void Start() { InitializeProperties(); }
-    private void InitializeProperties()
+    [Header("Character Data")]
+    public List<CharacterB> CharactersCollection;
+    public CharacterB CurrentCharacter;
+
+
+    public void Start()
+    {
+        InitializeScripts();
+        InitializeUIElements();
+        InitializeButtons();
+        InitializeCharacterData();
+    }
+    public void InitializeScripts()
+    {
+        CharactersTreeManager = GameObject.Find("Canvas/Menus").GetComponent<CharactersTreeManager>();
+        Powerup = GameObject.Find("Scripts/PowerUp").GetComponent<Powerup>();
+    }
+    public void InitializeUIElements()
     {
         Menu = GameObject.Find("Canvas/Menus/CharacterCenter");
-
-        CharactersCollection = new List<CharacterB>();
-        CurrentCharacter = null;
-
         NameText = Menu.transform.Find("Characters/Details/Name").GetComponent<TMP_Text>();
         DescriptionText = Menu.transform.Find("Characters/Details/Description/Text").GetComponent<TMP_Text>();
-
-        DefaultButton = Resources.Load<GameObject>("Buttons/Default");
-        DeleteButton = Menu.transform.Find("Characters/Details/Delete").gameObject;
-        DeleteButton.GetComponent<Button>().onClick.AddListener(() => DeleteCharacter());
-        GameplayButton = Menu.transform.Find("SwapScreen").gameObject;
 
         AttributesContentPanel = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Attributes/Buttons/ScrollView/ViewPort/Content").transform;
         MethodsContentPanel = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Methods/Buttons/ScrollView/ViewPort/Content").transform;
         SpecialAbilityContentPanel = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/SpecialAbility/Buttons/ScrollView/ViewPort/Content").transform;
+    }
+    public void InitializeButtons()
+    {
+        DefaultButton = Resources.Load<GameObject>("Buttons/Default");
 
-        CharactersTreeManager = GameObject.Find("Canvas/Menus").GetComponent<CharactersTreeManager>();
+        DeleteButton = Menu.transform.Find("Characters/Details/Delete").gameObject;
+        DeleteButton.GetComponent<Button>().onClick.AddListener(() => DeleteCharacter());
+
+        GameplayButton = Menu.transform.Find("SwapScreen").gameObject;
 
         GetButton = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Description/G");
         SetButton = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Description/S");
@@ -60,7 +71,11 @@ public class CharactersManager : MonoBehaviour
         AttributePlusButton = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Attributes/Buttons/Edit");
         MethodPlusButton = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Methods/Buttons/Edit");
     }
-
+    public void InitializeCharacterData()
+    {
+        CharactersCollection = new List<CharacterB>();
+        CurrentCharacter = null;
+    }
 
     public void AddCharacter(CharacterB builtCharacter)
     {
@@ -76,28 +91,20 @@ public class CharactersManager : MonoBehaviour
         Destroy(CurrentCharacter.CharacterButton.Button);
 
         CharactersTreeManager.BuildTree(CharactersCollection.First(), CharactersCollection.Last());
-        if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "OnlineBuilder")
-        {
-            GameObject.Find("Canvas/Popups").GetComponent<CharacterSelectionManager>().CleanContent();
-        }
 
+        if (SceneManager.GetActiveScene().name == "OnlineBuilder") GameObject.Find("Canvas/Popups").GetComponent<CharacterSelectionManager>().CleanContent();
         DisplayCharacter(CharactersCollection.Last());
     }
-
     public void DisplayCharacter(CharacterB displayCharacter)
     {
-        if (CurrentCharacter is not null)
-        {
-            GameObject previousCharacterObject = CurrentCharacter.CharacterButton.Button;
-            previousCharacterObject.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
-        }
+        if (CurrentCharacter != null) DeselectPreviousCharacter();
 
-        GameObject currentCharacterObject = displayCharacter.CharacterButton.Button;
-        currentCharacterObject.GetComponent<Image>().color = displayCharacter.IsAbstract ? new Color32(173, 216, 230, 255) : new Color32(255, 165, 0, 255);
-
+        SelectCurrentCharacter(displayCharacter);
         CurrentCharacter = CharactersCollection.Find(character => character == displayCharacter);
-        GameplayButton.GetComponent<Button>().interactable = CurrentCharacter.IsAbstract is false;
-        if (CurrentCharacter is not null)
+
+        GameplayButton.GetComponent<Button>().interactable = !CurrentCharacter.IsAbstract;
+
+        if (CurrentCharacter != null)
         {
             ClearContentPanels();
 
@@ -108,50 +115,64 @@ public class CharactersManager : MonoBehaviour
             DisplaySpecialAbility();
             DisplayDelete();
 
-            Powerup powerUp = GameObject.Find("Scripts/PowerUp").GetComponent<Powerup>();
-            powerUp.ApplyPowerup(CurrentCharacter);
+            ApplyPowerup();
 
             GetButton.SetActive(false);
             SetButton.SetActive(false);
 
-            if (UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "OnlineBuilder")
-                GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Value").GetComponent<AttributeValueManager>().SetAttribute(null);
-
-            if (!CurrentCharacter.IsOriginal || (SceneManager.GetActiveScene().name == "OnlineBuilder" && !GameObject.Find("Scripts/PlayTestManager").GetComponent<PlayTestManager>().IsTestGameplay))
-            {
-                AttributePlusButton.SetActive(true);
-                MethodPlusButton.SetActive(true);
-            }
-            else
-            {
-                AttributePlusButton.SetActive(false);
-                MethodPlusButton.SetActive(false);
-            }
+            if (SceneManager.GetActiveScene().name == "OnlineBuilder") GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Value").GetComponent<AttributeValueManager>().SetAttribute(null);
+            ToggleEditButtons();
         }
     }
-    private void DisplayName()
+    public void DeselectPreviousCharacter()
+    {
+        GameObject previousCharacterObject = CurrentCharacter.CharacterButton.Button;
+        previousCharacterObject.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+    }
+    public void SelectCurrentCharacter(CharacterB displayCharacter)
+    {
+        GameObject currentCharacterObject = displayCharacter.CharacterButton.Button;
+        currentCharacterObject.GetComponent<Image>().color = displayCharacter.IsAbstract ? new Color32(173, 216, 230, 255) : new Color32(255, 165, 0, 255);
+    }
+    public void ApplyPowerup()
+    {
+        Powerup.ApplyPowerup(CurrentCharacter);
+    }
+    public void ToggleEditButtons()
+    {
+        if (!CurrentCharacter.IsOriginal || (SceneManager.GetActiveScene().name == "OnlineBuilder" && !GameObject.Find("Scripts/PlayTestManager").GetComponent<PlayTestManager>().IsTestGameplay))
+        {
+            AttributePlusButton.SetActive(true);
+            MethodPlusButton.SetActive(true);
+        }
+
+        else
+        {
+            AttributePlusButton.SetActive(false);
+            MethodPlusButton.SetActive(false);
+        }
+    }
+    public void DisplayName()
     {
         NameText.text = (CurrentCharacter.IsAbstract ? " {ABST} " : "") + CurrentCharacter.Name;
         DescriptionText.text = CurrentCharacter.Description;
     }
-    private void DisplayAttributes()
+    public void DisplayAttributes()
     {
         foreach (Attribute attribute in CurrentCharacter.Attributes)
         {
             GameObject attributeGameObject = Instantiate(DefaultButton, AttributesContentPanel);
             attributeGameObject.AddComponent<DescriptionButton>();
-
             attributeGameObject.name = attribute.Name;
 
             TMP_Text buttonText = attributeGameObject.GetComponentInChildren<TMP_Text>();
             buttonText.text = attribute.Name;
 
             bool isAttributeOwner = CurrentCharacter.Name == attribute.Owner;
-            if (RestrictionManager.Instance.AllowAccessModifier && isAttributeOwner) { attributeGameObject.AddComponent<AccessModifierButton>().Attribute = attribute; }
+            if (RestrictionManager.Instance.AllowAccessModifier && isAttributeOwner) attributeGameObject.AddComponent<AccessModifierButton>().Attribute = attribute;
         }
-        
     }
-    private void DisplayMethods()
+    public void DisplayMethods()
     {
         foreach (Method method in CurrentCharacter.Methods)
         {
@@ -163,7 +184,7 @@ public class CharactersManager : MonoBehaviour
             buttonText.text = method.Name;
 
             bool isMethodOwner = CurrentCharacter.Name == method.Owner;
-            if (RestrictionManager.Instance.AllowAccessModifier && isMethodOwner) { methodGameObject.AddComponent<AccessModifierButton>().Method = method; }
+            if (RestrictionManager.Instance.AllowAccessModifier && isMethodOwner) methodGameObject.AddComponent<AccessModifierButton>().Method = method;
         }
     }
     public void DisplayUpcastMethod()
@@ -173,7 +194,11 @@ public class CharactersManager : MonoBehaviour
             for (int i = 0; i < MethodsContentPanel.childCount; i++)
             {
                 GameObject child = MethodsContentPanel.GetChild(i).gameObject;
-                if (CurrentCharacter.Methods.Any(method => method.Name == child.name) == false) { Destroy(child); break; }
+                if (!CurrentCharacter.Methods.Any(method => method.Name == child.name))
+                {
+                    Destroy(child);
+                    break;
+                }
             }
         }
 
@@ -191,7 +216,7 @@ public class CharactersManager : MonoBehaviour
             buttonText.text = CurrentCharacter.UpcastMethod.CharacterMethod.Name;
         }
     }
-    private void DisplaySpecialAbility()
+    public void DisplaySpecialAbility()
     {
         GameObject specialAbilityGameObject = Instantiate(DefaultButton, SpecialAbilityContentPanel);
         specialAbilityGameObject.AddComponent<DescriptionButton>();
@@ -199,15 +224,29 @@ public class CharactersManager : MonoBehaviour
 
         Image specialAbilityGameObjectImage = specialAbilityGameObject.GetComponent<Image>();
         specialAbilityGameObjectImage.color = new Color32(0, 255, 255, 255);
-        
+
         TMP_Text buttonText = specialAbilityGameObject.GetComponentInChildren<TMP_Text>();
         buttonText.text = CurrentCharacter.SpecialAbility?.Name;
     }
-    private void DisplayDelete() { DeleteButton.SetActive(CurrentCharacter.IsOriginal == false && CurrentCharacter.IsLeaf()); }
-    private void ClearContentPanels()
+    public void DisplayDelete()
     {
-        foreach (Transform child in AttributesContentPanel) Destroy(child.gameObject);
-        foreach (Transform child in MethodsContentPanel) Destroy(child.gameObject);
-        foreach (Transform child in SpecialAbilityContentPanel) Destroy(child.gameObject);
+        DeleteButton.SetActive(!CurrentCharacter.IsOriginal && CurrentCharacter.IsLeaf());
+    }
+    public void ClearContentPanels()
+    {
+        foreach (Transform child in AttributesContentPanel)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Transform child in MethodsContentPanel)
+        {
+            Destroy(child.gameObject);
+        }
+
+        foreach (Transform child in SpecialAbilityContentPanel)
+        {
+            Destroy(child.gameObject);
+        }
     }
 }
