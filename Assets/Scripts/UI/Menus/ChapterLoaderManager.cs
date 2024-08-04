bba6dea5ed2128b6c14ce81cd0190d1eca26aa8a
Nitzan <nitzantomer1998@gmail.com>
2024-08-04 13:48:25 +0300

    Updating Chapter Loader Manager Structure

--- Assets/Scripts/UI/Menus/ChapterLoaderManager.cs
+++ Assets/Scripts/UI/Menus/ChapterLoaderManager.cs
@@ -8,26 +8,39 @@ public class ChapterLoaderManager : MonoBehaviour
 {
     [Header("UI Elements")]
     public List<Button> LevelButtons;
+
+    [Header("Chapter Information")]
     public int ChapterIndex;
+
 
     public void Start()
     {
+        InitializeGameplayData();
+
+        LoadChapterInfo();
+    }
+    public void InitializeGameplayData()
+    {
         GameplayData.Initialize();
         GameplayData.Load();
+    }
+
+    public void LoadChapterInfo()
+    {
         var gameplayInfo = SceneManagement.GameplayInfo;
         var chapterInfos = gameplayInfo[0].ChapterInfos;
         var levelsInfo = ChapterIndex == -1 ? chapterInfos.Select(ci => ci.LevelsInfo[0]).ToList() : chapterInfos[ChapterIndex].LevelsInfo;
 
+        UpdateLevelButtons(levelsInfo);
+    }
+    public void UpdateLevelButtons(List<LevelInfo> levelsInfo)
+    {
         for (int i = 0; i < LevelButtons.Count; i++)
         {
             var levelStatus = levelsInfo[i].Status;
             LevelButtons[i].interactable = levelStatus != -1;
 
-            if (levelStatus == 1)
-            {
-                LevelButtons[i].GetComponent<Image>().color = Color.green;
-            }
+            if (levelStatus == 1) LevelButtons[i].GetComponent<Image>().color = Color.green;
         }
     }
-
 }
