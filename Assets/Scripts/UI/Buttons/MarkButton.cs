384b0ebcf01c5f1d2ed8e0259d6a30452e075b27
Nitzan <nitzantomer1998@gmail.com>
2024-08-04 13:49:49 +0300

    Updating Mark Button  Structure

--- Assets/Scripts/UI/Buttons/MarkButton.cs
+++ Assets/Scripts/UI/Buttons/MarkButton.cs
@@ -1,54 +1,73 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 
 public class MarkButton : MonoBehaviour
 {
+    [Header("Buttons")]
     public GameObject GetButton;
     public GameObject SetButton;
     public GameObject AllButton;
+
+    [Header("Attribute")]
     public Attribute Attribute;
 
-    public void Start() { InitializeProperties(); }
-    private void InitializeProperties()
+
+    public void Start()
+    {
+        InitializeButtons();
+    }
+    public void InitializeButtons()
     {
         GetButton = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Description/G");
+        GetButton.GetComponent<Button>().onClick.AddListener(GetterMark);
+
         SetButton = GameObject.Find("Canvas/Menus/CharacterCenter/Characters/Details/Description/S");
         SetButton.GetComponent<Button>().onClick.AddListener(SetterMark);
-        GetButton.GetComponent<Button>().onClick.AddListener(GetterMark);
     }
 
     public void SetterMark()
     {
         Attribute.Setter = !Attribute.Setter;
+
         SetButton.GetComponent<Image>().color = Attribute.Setter ? Color.green : Color.white;
     }
-
     public void GetterMark()
     {
         Attribute.Getter = !Attribute.Getter;
+
         GetButton.GetComponent<Image>().color = Attribute.Getter ? Color.green : Color.white;
     }
-
     public void AttributeClicked(Attribute attribute)
     {
         Attribute = attribute;
+
+        UpdateButtonColors();
+    }
+    public void UpdateButtonColors()
+    {
         GetButton.GetComponent<Image>().color = Attribute.Getter ? Color.green : Color.white;
         SetButton.GetComponent<Image>().color = Attribute.Setter ? Color.green : Color.white;
     }
-
     public void ActivateButtons()
     {
-        GetButton.SetActive(true);
-        SetButton.SetActive(true);
-        GetButton.GetComponent<Button>().interactable = !CharactersData.CharactersManager.CurrentCharacter.IsOriginal || RestrictionManager.Instance.OnlineBuild;
-        SetButton.GetComponent<Button>().interactable = !CharactersData.CharactersManager.CurrentCharacter.IsOriginal || RestrictionManager.Instance.OnlineBuild;
+        SetButtonsActive(true);
+        UpdateButtonInteractivity();
     }
-
     public void DeactivateButtons()
     {
-        GetButton.SetActive(false);
-        SetButton.SetActive(false);
+        SetButtonsActive(false);
+    }
+    public void SetButtonsActive(bool isActive)
+    {
+        GetButton.SetActive(isActive);
+        SetButton.SetActive(isActive);
+    }
+    public void UpdateButtonInteractivity()
+    {
+        bool isInteractable = !CharactersData.CharactersManager.CurrentCharacter.IsOriginal || RestrictionManager.Instance.OnlineBuild;
+
+        GetButton.GetComponent<Button>().interactable = isInteractable;
+        SetButton.GetComponent<Button>().interactable = isInteractable;
     }
 }
